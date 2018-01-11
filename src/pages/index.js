f6d41fd09e3abe566556b6c53975bb13bd46b125
Steven Miyakawa <steven.miyakawa@here.com>
2018-01-10 23:07:37 -0800

    Add logo date and location

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import blackLogo from '../assets/reactathon-logo-black.svg'
 
-const IndexPage = () => <div />
+const IndexPage = () => (
+  <main style={{ textAlign: 'center', marginTop: 100 }}>
+    <img src={blackLogo} alt="Reactathon logo"/>
+    <h1 style={{ marginTop: 30 }}>March 20-22, 2018</h1>
+    <h2>San Francisco</h2>
+  </main>
+)
 
 export default IndexPage
