b8a1d5cc68e010bed6f9ca225c9694996838a8f7
Steven Miyakawa <steven.miyakawa@here.com>
2018-02-02 18:30:33 -0500

    Display smaller header on all pages besides home page

---
+++ src/components/SmallHeader/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styles from './smallHeader.module.css'
+import whiteLogo from '../../assets/reactathon-logo-white.svg'
+
+const SmallHeader = () => (
+  <header className={styles.header}>
+    <img src={whiteLogo} alt="Reactathon logo" className={styles.logo} />
+    <nav className={styles.nav}>
+      <Link to="/">Home</Link>
+      <Link to="/conference">Conference</Link>
+      <Link to="/workshops">Workshops</Link>
+      <Link to="/hackathon">Hackathon</Link>
+      <Link to="/hiring-mixer">Hiring Mixer</Link>
+    </nav>
+  </header>
+)
+
+export default SmallHeader
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,36 +2,41 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import BigHeader from '../components/BigHeader'
+import SmallHeader from '../components/SmallHeader'
 import Footer from '../components/Footer'
 import './index.css'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Reactathon"
-      meta={[
-        { name: 'description', content: 'Reactathon 2018' },
-        {
-          name: 'keywords',
-          content:
-            'react, reactjs, react.js, react conf, workshop, netlify, lyft, github, opentable, eventebrite, webpack, redux, learn to code, learn to program, learn react, san francisco, reactathon, learn programming, learn javascript, learn coding, code, coding, programming, software engineer, software developer, web development, development, engineering, coding bootcamp, javascript, open source',
-        },
-      ]}
-    />
-    <BigHeader />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 1080,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+const TemplateWrapper = ({ children }) => {
+  const isRootPage = window.location.pathname === '/';
+
+  return (
+    <div>
+      <Helmet
+        title="Reactathon"
+        meta={[
+          { name: 'description', content: 'Reactathon 2018' },
+          {
+            name: 'keywords',
+            content:
+              'react, reactjs, react.js, react conf, workshop, netlify, lyft, github, opentable, eventebrite, webpack, redux, learn to code, learn to program, learn react, san francisco, reactathon, learn programming, learn javascript, learn coding, code, coding, programming, software engineer, software developer, web development, development, engineering, coding bootcamp, javascript, open source',
+          },
+        ]}
+      />
+      {isRootPage ? <BigHeader /> : <SmallHeader />}
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 1080,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0,
+        }}
+      >
+        {children()}
+      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-)
+  )
+}
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
