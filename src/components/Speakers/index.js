5e30679397c26fe45d903c20ee6ec0b4b8a1fdc0
Steven Miyakawa <steven.miyakawa@here.com>
2018-02-04 18:09:19 -0800

    Refactor out speaker data into a separate file

--- src/components/Speakers/index.js
+++ src/components/Speakers/index.js
@@ -1,193 +1,9 @@
 import React from 'react'
-import benIlegbodu from './images/ben-ilegbodu@3x.png'
-import brianLeroux from './images/brian-leroux@3x.png'
-import christinaHolland from './images/christina-holland@3x.png'
-import davidWells from './images/david-wells@3x.png'
-import ericaCooksey from './images/erica-cooksey@3x.png'
-import featherKnee from './images/feather-knee@3x.png'
-import harryTormey from './images/harry-tormey@3x.png'
-import jaredForsyth from './images/jared-forsyth@3x.png'
-import jayPhelps from './images/jay-phelps@3x.png'
-import joeSeifi from './images/joe-seifi@3x.png'
-import johannesSchickling from './images/johannes-schickling@3x.png'
-import kentCDodds from './images/kent-c-dodds@3x.png'
-import markErikson from './images/mark-erikson@3x.png'
-import mikeGrabowski from './images/mike-grabowski@3x.png'
-import parashuramNarasimhan from './images/parashuram-narasimhan@3x.png'
-import rogelioGuzman from './images/rogelio-guzman@3x.png'
-import sashkoStubailo from './images/sashko-stubailo@3x.png'
-import secondPhotoPlaceholder from './images/secondPhotoPlaceholder@3x.png'
-import sophiaShoemaker from './images/sophia-shoemaker@3x.png'
-import swizecTeller from './images/swizec-teller@3x.png'
-import tylerMartinez from './images/tyler-martinez@3x.png'
-import mattBiilman from './images/matt-biilmann@3x.png'
-import kyleMatthews from './images/kyle-mathews@3x.png'
-import brianDouglas from './images/brian-douglas@3x.png'
+import speakers from './speakers.json'
 import gitHubIcon from './github.svg'
 import twitterIcon from './twitter.svg'
-
-const data = [
-  {
-    name: 'Ben Ilegbodu',
-    company: 'Eventbrite',
-    twitter: 'https://www.twitter.com/benmvp',
-    github: 'https://www.github.com/benmvp',
-    imgSrc: benIlegbodu,
-  },
-  {
-    name: 'Kent C. Dodds',
-    company: 'Paypal',
-    twitter: 'https://www.twitter.com/kentcdodds',
-    github: 'https://www.github.com/kentcdodds',
-    imgSrc: kentCDodds,
-  },
-  {
-    name: 'Sophia Shoemaker',
-    company: 'Fullstack.io',
-    twitter: 'https://www.twitter.com/wisecobbler',
-    github: 'https://github.com/mrscobbler',
-    imgSrc: sophiaShoemaker,
-  },
-  {
-    name: 'Parashuram Narasimhan',
-    company: 'Facebook',
-    imgSrc: parashuramNarasimhan,
-  },
-  {
-    name: 'Jay Phelps',
-    company: 'Netflix',
-    twitter: 'https://www.twitter.com/swizec',
-    github: 'https://github.com/swizec',
-    imgSrc: jayPhelps,
-  },
-  {
-    name: 'Swizec Teller',
-    company: 'Yup Technologies',
-    twitter: 'https://www.twitter.com/swizec',
-    github: 'https://github.com/swizec',
-    imgSrc: swizecTeller,
-  },
-  {
-    name: 'Johannes Schickling',
-    company: 'GraphCool',
-    twitter: 'https://www.twitter.com/_schickling',
-    github: 'https://github.com/schickling',
-    imgSrc: johannesSchickling,
-  },
-  {
-    name: 'Feather Knee',
-    company: 'Independent',
-    twitter: 'https://www.twitter.com/featherart',
-    github: 'https://github.com/featherart',
-    imgSrc: featherKnee,
-  },
-  {
-    name: 'Mike Grabowski',
-    company: 'Callstack',
-    twitter: 'https://www.twitter.com/grabbou',
-    github: 'https://github.com/grabbou',
-    imgSrc: mikeGrabowski,
-  },
-  {
-    name: 'Christina Holland',
-    company: 'Pepperdata',
-    twitter: 'https://www.twitter.com/hsubox76',
-    github: 'https://github.com/americanwombat',
-    imgSrc: christinaHolland,
-  },
-  {
-    name: 'Brian Leroux',
-    company: 'Begin.com',
-    twitter: 'https://www.twitter.com/brianleroux',
-    github: 'https://github.com/brianleroux',
-    imgSrc: brianLeroux,
-  },
-  {
-    name: 'Harry Tormey',
-    company: 'Launchdrawer',
-    twitter: 'https://www.twitter.com/htormey',
-    github: 'https://github.com/hgale',
-    imgSrc: harryTormey,
-  },
-  {
-    name: 'Erica Cooksey',
-    company: 'Eaze',
-    twitter: 'https://www.twitter.com/EricaCooksey',
-    github: 'https://github.com/ericacooksey',
-    imgSrc: ericaCooksey,
-  },
-  {
-    name: 'Matt Biilman',
-    company: 'Netlify',
-    twitter: 'https://www.twitter.com/biilmann',
-    github: 'https://github.com/biilmann',
-    imgSrc: mattBiilman,
-  },
-  {
-    name: 'Joe Seifi',
-    company: 'Eventbrite',
-    twitter: 'https://www.twitter.com/joeshub',
-    github: 'https://github.com/joeseifi',
-    imgSrc: joeSeifi,
-  },
-  {
-    name: 'Mark Erikson',
-    company: 'Northrup Gruman',
-    twitter: 'https://www.twitter.com/acemark',
-    github: 'https://github.com/markerikson',
-    imgSrc: markErikson,
-  },
-  {
-    name: 'Sashko Stubailo',
-    company: 'Apollo',
-    twitter: 'https://www.twitter.com/stubailo',
-    github: 'https://github.com/stubailo',
-    imgSrc: sashkoStubailo,
-  },
-  {
-    name: 'Joe Lynch',
-    company: 'Sony Interactive Ent.',
-    twitter: 'https://www.twitter.com/',
-    github: 'https://github.com/',
-    imgSrc: secondPhotoPlaceholder,
-  },
-
-  {
-    name: 'Tyler Martinez',
-    company: 'Docker',
-    twitter: 'https://www.twitter.com/slightlytyler',
-    github: 'https://github.com/slightlytyler',
-    imgSrc: tylerMartinez,
-  },
-  {
-    name: 'Jared Forsyth',
-    company: 'Khan Academy',
-    twitter: 'https://www.twitter.com/jaredforsyth',
-    github: 'https://github.com/jaredly',
-    imgSrc: jaredForsyth,
-  },
-  {
-    name: 'David Wells',
-    company: 'Severless',
-    twitter: 'https://www.twitter.com/davidwells',
-    github: 'https://github.com/davidwells',
-    imgSrc: davidWells,
-  },
-  {
-    name: 'Kyle Matthews',
-    company: 'Gatsby',
-    twitter: 'https://www.twitter.com/kylemathews',
-    github: 'https://github.com/kyleamathews',
-    imgSrc: kyleMatthews,
-  },
-  {
-    name: 'Rogelio Guzman',
-    company: 'Docker',
-    twitter: 'https://www.twitter.com/rogeliog',
-    github: 'https://github.com/rogeliog',
-    imgSrc: rogelioGuzman,
-  },
-]
+import brianDouglas from './images/brian-douglas@3x.png'
+import secondPhotoPlaceholder from './images/secondPhotoPlaceholder@3x.png'
 
 const Speaker = ({ name, company, twitter, github, imgSrc }) => (
   <div style={{ textAlign: 'center', width: 250, marginBottom: 50 }}>
@@ -211,14 +27,14 @@ const Speakers = () => (
     <div
       style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}
     >
-      {data.map(({ name, company, twitter, github, imgSrc }, i) => (
+      {speakers.map(({ name, company, twitter, github, imgSlug }, i) => (
         <Speaker
           key={`speaker-${i}`}
           name={name}
           company={company}
           twitter={twitter}
           github={github}
-          imgSrc={imgSrc}
+          imgSrc={imgSlug ? require(`./images/${imgSlug}@3x.png`) : secondPhotoPlaceholder}
         />
       ))}
     </div>
