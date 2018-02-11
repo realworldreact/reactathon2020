0405ce1b2b955e39bfc061992f6118d22e9bed9a
Steven Miyakawa <steven.miyakawa@here.com>
2018-02-11 13:36:24 -0800

    Fade in big header

--- src/components/BigHeader/index.js
+++ src/components/BigHeader/index.js
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'gatsby-link'
+import Fade from 'react-reveal/Fade'
 import styles from './bigHeader.module.css'
 import whiteLogo from './splash-logo@3x.png'
 import BuyTicketsButton from '../BuyTicketsButton'
 
-const BigHeader = () => (
-  <header className={styles.header}>
-    <nav className={styles.nav}>
-      <Link to="/">Home</Link>
-      <Link to="/conference">Conf Schedule</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/hackathon">Hackathon</Link>
-      <Link to="/hiring-mixer">Hiring Mixer</Link>
-    </nav>
-    <div className={styles['main-container']}>
-      <img
-        src={whiteLogo}
-        alt="Reactathon white logo"
-        className={styles.logo}
-      />
-      <h1 style={{ marginTop: 30, color: 'white' }}>March 20-22, 2018</h1>
-      <h2 style={{ marginBottom: 25, color: 'white' }}>San Francisco</h2>
-      <BuyTicketsButton />
-    </div>
-  </header>
-)
+class BigHeader extends Component {
+  state = {
+    loaded: false,
+  }
+
+  render() {
+    return (
+      <Fade onReveal={() => this.setState({ loaded: true })}>
+        <header className={styles.header}>
+          <nav className={styles.nav}>
+            <Link to="/">Home</Link>
+            <Link to="/conference">Conf Schedule</Link>
+            <Link to="/workshops">Workshops</Link>
+            <Link to="/hackathon">Hackathon</Link>
+            <Link to="/hiring-mixer">Hiring Mixer</Link>
+          </nav>
+          <div className={styles['main-container']}>
+            <img
+              src={whiteLogo}
+              alt="Reactathon white logo"
+              className={styles.logo}
+            />
+            <h1 style={{ marginTop: 30, color: 'white' }}>March 20-22, 2018</h1>
+            <h2 style={{ marginBottom: 25, color: 'white' }}>San Francisco</h2>
+            {this.state.loaded && <BuyTicketsButton fadeIn />}
+          </div>
+        </header>
+      </Fade>
+    )
+  }
+}
 
 export default BigHeader
--- src/components/BuyTicketsButton/index.js
+++ src/components/BuyTicketsButton/index.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade'
 import classNames from 'classnames'
 import styles from './buyTicketsButton.module.css'
 import { Link } from 'react-scroll'
 import { BUY_TICKETS_ID } from '../../constants'
 
-const BuyTicketsButton = ({ inverse, text = 'Buy Tickets' }) => (
-  <div className={classNames({ [styles.inverse]: inverse })}>
-    <Link className={styles.btn} to={BUY_TICKETS_ID} smooth>
-      {text}
-    </Link>
-  </div>
-)
+const BuyTicketsButton = ({
+  inverse,
+  text = 'Buy Tickets',
+  fadeIn = false,
+}) => {
+  const children = (
+    <div className={classNames({ [styles.inverse]: inverse })}>
+      <Link className={styles.btn} to={BUY_TICKETS_ID} smooth>
+        {text}
+      </Link>
+    </div>
+  )
+
+  return fadeIn ? <Fade delay={500}>{children}</Fade> : children
+}
 
 export default BuyTicketsButton
