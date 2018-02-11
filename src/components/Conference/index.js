0a51da22dd4033c47daf092d594fb0c73020133a
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2018-02-11 15:20:26 -0800

    shorten animation load-in for home page sub-banner descriptions

--- src/components/Conference/index.js
+++ src/components/Conference/index.js
@@ -28,7 +28,7 @@ const Conference = () => (
           className="icon"
         />
         <h1 style={{ marginBottom: 30 }}>Fundamentals</h1>
-        <Fade delay={3000}>
+        <Fade delay={1200}>
           <p>
             A full day of talks filled with fundamentals, best practices, and
             contextual advice in a single-day, single-track conference
@@ -42,7 +42,7 @@ const Conference = () => (
           className="icon"
         />
         <h1 style={{ marginBottom: 30 }}>Advanced</h1>
-        <Fade delay={5000}>
+        <Fade delay={2000}>
           <p>
             Two full days of talks, mingling, and new friends built around the
             cutting-edge of web and mobile development
