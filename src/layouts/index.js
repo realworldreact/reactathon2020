2a63d70c8c2ae46f736c47f8b734af5f90fe1a71
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2018-02-12 07:44:32 -0800

    add meta content and twitter cards

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,14 +13,69 @@ const TemplateWrapper = ({ children, location }) => {
   return (
     <div>
       <Helmet
-        title="Reactathon"
+        title="Reactathon 2018"
         meta={[
-          { name: 'description', content: 'Reactathon 2018' },
+          {
+            name: 'description',
+            content:'3-day dual-conference & hackathon in San Francisco featuring the brightest minds in the JS/React community.'
+          },
           {
             name: 'keywords',
             content:
-              'react, reactjs, react.js, react conf, workshop, netlify, lyft, github, opentable, eventebrite, webpack, redux, learn to code, learn to program, learn react, san francisco, reactathon, learn programming, learn javascript, learn coding, code, coding, programming, software engineer, software developer, web development, development, engineering, coding bootcamp, javascript, open source',
+              'react, reactjs, react.js, react conf, conference, conf, workshop, netlify, github, eventbrite, webpack, redux, workshop, docker, learn to code, learn to program, learn react, san francisco, reactathon, learn programming, learn javascript, learn coding, code, coding, programming, software engineer, software developer, web development, development, engineering, coding bootcamp, javascript, open source'
           },
+          {
+            content: 'Reactathon',
+            property: 'og:site_name'
+          },
+          {
+            content: 'on',
+            name: 'twitter:widgets:csp'
+          },
+          {
+            content: 'https://www.reactathon.com',
+            property: 'og:url'
+          },
+          {
+            content: '3-day dual-conference & hackathon in San Francisco featuring the brightest minds in the JS/React community.',
+            property: 'og:description'
+          },
+          {
+            content: 'https://s3-us-west-1.amazonaws.com/reactathon-2018/reactathon-og-image.png',
+            property: 'og:image'
+          },
+          {
+            content: 'article',
+            property: 'og:type'
+          },
+          {
+            content: 'https://www.facebook.com/realworldreact',
+            property: 'article:publisher'
+          },
+          {
+            content: '@reactathon',
+            name: 'twitter:creator'
+          },
+          {
+            content: 'https://www.reactathon.com',
+            name: 'twitter:url'
+          },
+          {
+            content: '@reactathon',
+            name: 'twitter:site'
+          },
+          {
+            content: 'summary_large_image',
+            name: 'twitter:card'
+          },
+          {
+            content: 'https://s3-us-west-1.amazonaws.com/reactathon-2018/reactathon-twitter-card.png',
+            name: 'twitter:image:src'
+          },
+          {
+            content: 'Reactathon: March 19 - 25',
+            name: 'twitter:title'
+          }
         ]}
         link={[{ rel: 'icon', href: favIcon }]}
       />
