41d000e7f27fcd80cfbf011e7f20b3079325568e
Steven Miyakawa <steven.miyakawa@here.com>
2018-02-11 20:11:55 -0800

    Update hiring mixer ticket widget ID

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ export const FUNDAMENTALS_TICKET_WIDGET_ID =
 export const ADVANCED_TICKET_WIDGET_ID =
   'reactathon-advanced-conference-tickets-san-francisco-MV7YF6'
 export const HIRING_MIXER_TICKET_WIDGET_ID =
-  'hiring-mixer-tickets-san-francisco-BTD5GW'
+  'reactathon-hiring-mixer-tickets-san-francisco-0LYH8Z'
 
 // TODO: Replace with ID for hackathon widget ID
 export const HACKATHON_TICKET_WIDGET_ID =
