3d44574cead0c49acf0f4f73d8a503245ed07040
Steven Miyakawa <steven.miyakawa@here.com>
2018-02-11 19:19:34 -0800

    Add new sponsor logos

--- src/components/Sponsors/index.js
+++ src/components/Sponsors/index.js
@@ -1,22 +1,31 @@
 import React from 'react'
-import styles from './index.module.css'
+import styles from './sponsors.module.css'
 import githubLogo from './github@2x.png'
 import microsoftLogo from './microsoft@2x.png'
+import dockerLogo from './docker@3x.png'
+import medidataLogo from './medidata@3x.png'
+import netflixLogo from './netflix@3x.png'
+
+const SponsorImage = ({ name, imgSrc, style }) => (
+  <img
+    src={imgSrc}
+    alt={`${name} logo`}
+    className={styles['sponsor-logo']}
+    style={style}
+  />
+)
 
 const Sponsors = () => (
   <section style={{ marginTop: 50, marginBottom: 100 }}>
     <h1 style={{ marginBottom: 100 }}>Sponsors</h1>
-    <div style={{ marginTop: 10 }}>
-      <img
-        src={githubLogo}
-        alt="GitHub logo"
-        className={styles['sponsor-logo']}
-      />
-      <img
-        src={microsoftLogo}
-        alt="Microsoft logo"
-        className={styles['sponsor-logo']}
-      />
+    <div style={{ marginBottom: 25 }}>
+      <SponsorImage name="GitHub" imgSrc={githubLogo} />
+      <SponsorImage name="Microsoft" imgSrc={microsoftLogo} />
+    </div>
+    <div>
+      <SponsorImage name="Docker" imgSrc={dockerLogo} />
+      <SponsorImage name="Medidata" imgSrc={medidataLogo} />
+      <SponsorImage name="Netflix" imgSrc={netflixLogo} />
     </div>
   </section>
 )
