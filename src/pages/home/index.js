60c4c3e993ff835545c3ad07512ab62c19fae103
Prashanth R <prashanthrdev@gmail.com>
2020-01-13 21:51:23 -0800

    header banner footer

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,7 +2,16 @@ import React from 'react'
 import PageContentLayout from '../../layouts/page-content'
 import Banner from '../../components/Banner'
 import CalIcon from '../../assets/icons/home/calendar-icon-home.svg'
+import FlexportLogo from '../../assets/images/home/header/header-flexport-logo.svg'
+import RWRLogo from '../../assets/images/home/header/header-rwr-logo.svg'
 import './index.css'
+
+const HomeBannerFooterSponsorImg = ({ src, className }) => (
+  <img 
+    className={className}
+    src={src}
+  />
+)
 
 const HomeBannerContent = () => (
   <div className='home-banner-content'>
@@ -18,12 +27,23 @@ const HomeBannerContent = () => (
       March 30 - 31, 2020
     </div>
     <div className='home-banner-cta'>
-      <button className='home-banner-cta-btn'>
-        Buy Tickets
-      </button>
+      <a href='#'>
+        <button className='home-banner-cta-btn'>
+          Buy Tickets
+        </button>
+      </a>
     </div>
     <div className='home-banner-footer'>
       Presented by 
+        <HomeBannerFooterSponsorImg 
+          className= 'home-banner-footer-sponsor-img home-banner-footer-sponsor-img-rwr' 
+          src={RWRLogo} 
+        />
+      &
+        <HomeBannerFooterSponsorImg 
+          className='home-banner-footer-sponsor-img home-banner-footer-sponsor-img-flexport' 
+          src={FlexportLogo} 
+        />
     </div>
   </div>
 )
