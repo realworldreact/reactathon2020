80cb6f99a6aa2ef65c4bcb3c484611ad5abb8ea5
Prashanth R <prashanthrdev@gmail.com>
2020-01-14 19:53:12 -0800

    css grid ftw

--- src/components/Home/Features/Grid/index.js
+++ src/components/Home/Features/Grid/index.js
@@ -1,8 +1,71 @@
 import React from 'react'
 import './index.css'
 
-const FeaturesGrid = () => (
-  <span>Hello world!</span>
+const FeatureGridItem = ({ className, imgSrc, imgAlt, title, description, ctaUrl }) => (
+  <div class={`feature ${className}`}>
+    <img src={imgSrc} alt={imgAlt} />
+    <div class='feature-copy'>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <p>
+        <a href={ctaUrl} class='btn-primary'>Learn more</a>
+      </p>
+    </div>
+  </div>
 )
+
+const FeaturesGrid = ({ features }) => (
+  <div class='features-grid'>
+      {features.map(feature => (
+        <FeatureGridItem 
+          imgSrc={feature.imgSrc}
+          imgAlt={feature.imgAlt}
+          title={feature.title}
+          description={feature.description}
+          ctaUrl={feature.ctaUrl}
+        />
+      ))}
+  </div>
+)
+
+FeaturesGrid.defaultProps = {
+  features: [{
+    description: 'test',
+    title: 'test',
+    imgSrc: 'https://www.reactathon.com/img/events/reception.svg',
+    imgAlt: 'test',
+    ctaUrl: 'test'
+  }, {
+    description: 'test',
+    title: 'test',
+    imgSrc: 'https://www.reactathon.com/img/events/reception.svg',
+    imgAlt: 'test',
+    ctaUrl: 'test'
+  }, {
+    description: 'test',
+    title: 'test',
+    imgSrc: 'https://www.reactathon.com/img/events/reception.svg',
+    imgAlt: 'test',
+    ctaUrl: 'test'
+  }, {
+    description: 'test',
+    title: 'test',
+    imgSrc: 'https://www.reactathon.com/img/events/reception.svg',
+    imgAlt: 'test',
+    ctaUrl: 'test'
+  }, {
+    description: 'test',
+    title: 'test',
+    imgSrc: 'https://www.reactathon.com/img/events/reception.svg',
+    imgAlt: 'test',
+    ctaUrl: 'test'
+  }, {
+    description: 'test',
+    title: 'test',
+    imgSrc: 'https://www.reactathon.com/img/events/reception.svg',
+    imgAlt: 'test',
+    ctaUrl: 'test'
+  }]
+}
 
 export default FeaturesGrid
