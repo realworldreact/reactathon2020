965d5f7f606a69775bd0a31aa8e146afa66fea5a
Prashanth R <prashanthrdev@gmail.com>
2020-01-19 12:39:28 -0800

    add empty highlights section

--- src/components/Home/Highlights/index.js
+++ src/components/Home/Highlights/index.js
@@ -2,9 +2,37 @@ import React from 'react'
 import SectionTitle from '../SectionTitle'
 import './index.css'
 
+const HighlightGalleryImage = ({ imgSrc, imgAlt, className = '' }) => (
+  <img 
+    src={imgSrc}
+    alt={imgAlt}
+    className={className}
+  />
+)
+
+const HighlightGallery = ({ images }) => (
+  <div className='highlight-gallery'>
+    {images.map(image => (
+      <HighlightGalleryImage 
+        className='highlight-gallery-image'
+        imgSrc={image.src}
+        imgAlt={image.alt}
+      /> 
+    ))}
+  </div>
+)
+
+HighlightGallery.defaultProps = {
+  images: [{
+    src: ''
+    alt: ''
+  }]
+}
+
 const Highlights = () => (
   <section className='section-highlights'>
     <SectionTitle text='Highlights' className='title-highlights' />
+    <HighlightGallery />
   </section>
 )
 
