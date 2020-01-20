6a1ddeadaf8900d591409ee2cf683450db39a687
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-01-20 13:13:44 -0800

    feat(12): Add home sponsors section (#16)
    * remove css
    
    * fix compilation
    
    * add sponsors

---
+++ src/components/Home/Sponsors/Grid/index.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import FlexportLogo from '../../../../assets/images/home/sponsors/sponsor-flexport.svg'
+import LendingClubLogo from '../../../../assets/images/home/sponsors/sponsor-lending-club.svg'
+import NetflixLogo from '../../../../assets/images/home/sponsors/sponsor-netflix.svg'
+import './index.css'
+
+const SPONSOR_EMAIL = 'sponsors@reactathon.com'
+
+const Sponsor = ({ title, logos = [], description = ''}) => (
+  <div className='sponsor-grid-sponsor'>
+    <SponsorTitle text={title} />
+    <div className='sponsor-grid-sponsor-logos'>
+      {logos.map(logo => (
+        <a target={logo.href && 'blank'} href={logo.href || '#'}>
+          <img 
+            className='sponsor-grid-sponsor-logo' 
+            src={logo.src} 
+            alt={logo.alt} 
+          />
+        </a>
+      ))}
+    </div>
+    {description && (
+      <div className='sponsor-grid-sponsor-description-wrap'>
+        <div className='sponsor-grid-sponsor-description'>
+          {description}
+        </div>
+      </div>
+    )}
+  </div>
+)
+
+const SponsorTitle = ({ text }) => (
+  <h2 className='sponsor-grid-sponsor-title'>
+    {text}
+  </h2>
+)
+
+const SponsorFooter = () => (
+  <div className='sponsor-grid-footer'>
+    <div className='sponsor-grid-footer-content'>
+      <p className='sponsor-grid-footer-content-bold'>
+        Interested in sponsoring the React.js event of the year?
+      </p>
+      <p>Request the sponsorship guide at</p>
+      <a 
+        className='sponsor-grid-footer-content-link' 
+        href={`mailto:${SPONSOR_EMAIL}`}>
+          {SPONSOR_EMAIL}
+      </a>
+    </div>
+  </div>
+)
+
+const SponsorGrid = ({ sponsors }) => (
+  <div className='sponsor-grid'>
+    {sponsors.map(sponsor => (
+      <Sponsor
+        title={sponsor.title}
+        logos={sponsor.logos}
+        description={sponsor.description}
+      />
+    ))}
+    <SponsorFooter />
+  </div>
+)
+
+SponsorGrid.defaultProps = {
+  sponsors: [{
+    title: 'Presenting Sponsor',
+    logos: [{
+      src: FlexportLogo,
+      alt: 'flexport-logo',
+      href: 'https://flexport.com/careers'
+    }],
+    description: `
+      At Flexport, we're building the technology that's simplifying global trade. 
+      We're making it easier for organizations around the worldto move their cargo from
+      one place to another - and that takes a lot of Javascript! If you'd like to help
+      us out with that responsibility, check us out! We're heavy React users.
+    `,
+  }, {
+    title: 'Silver Sponsors',
+    logos: [{
+      src: LendingClubLogo,
+      alt: 'lending-club'
+    }], 
+  }, {
+    title: 'Community Sponsors',
+    logos: [{
+      src: NetflixLogo,
+      alt: 'netflix-logo'
+    }]
+  }]
+}
+
+export default SponsorGrid
--- src/components/Home/Sponsors/index.js
+++ src/components/Home/Sponsors/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import SectionTitle from '../SectionTitle'
+import SponsorGrid from './Grid'
 import './index.css'
 
 const Sponsors = () => (
   <section className='section-sponsors'>
     <SectionTitle text='Sponsors' className='title-sponsors' />
+    <SponsorGrid />
   </section>
 )
 
