356f997ce47f25c8b9affe639f4a3b0dca4dfe3a
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-01-25 13:49:36 -0800

    fix(json): Fix json data and add parsers (#32)

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,12 @@ module.exports = {
   },
   plugins: [
       'gatsby-plugin-react-helmet',
+      'gatsby-transformer-json',
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `./src/assets/data`,
+        },
+      },
     ],
 };
