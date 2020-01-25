7b72732775b71d9b8cd8a5d547867edfeb7ce066
Prashanth R <prashanthrdev@gmail.com>
2020-01-25 13:48:34 -0800

    fix(json): Fix json data and add parsers

--- src/components/About/Section/TopicTables/index.js
+++ src/components/About/Section/TopicTables/index.js
@@ -3,6 +3,7 @@ import SubPageSectionHeader from '../../../SubPageSectionHeader'
 import DateIcon from '../../../../assets/icons/about/calendar-icon-about.svg'
 import TimeIcon from '../../../../assets/icons/about/clock-icon-about.svg'
 import Topics from './Topics'
+import data from '../../../../assets/data/about/topic-tables.json'
 import './index.css'
 
 
@@ -41,19 +42,11 @@ const TopicTables = ({ header, description, topicHeader, listItems, date, time }
 )
 
 TopicTables.defaultProps = {
-  header: 'Topic Tables',
-  description: `
-    A gathering place where engineers test & expand their knowledge of a 
-    subject through discussion, moderated by experts.
-  `,
-  listItems: [
-    'Easily identify the subjects you care about',
-    'Break the ice with the other attendees, speakers, and workshop instructors',
-    'Engage in conversation or listen to others discuss',
-    'Make new friends & improve your confidence through experience'
-  ],
-  date: 'March 30',
-  time: '6pm-8pm'
+  header: data.section.h2Slab,
+  description: data.section.p1,
+  listItems: data.section.bullets,
+  date: data.section.date,
+  time: data.section.time
 }
 
 export default TopicTables
