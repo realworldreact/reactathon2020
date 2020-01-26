1be47acc44e55af31ed76cc06b9d62dbde952edd
Prashanth R <prashanthrdev@gmail.com>
2020-01-26 14:12:55 -0800

    make flex nav

--- src/components/About/Nav/index.js
+++ src/components/About/Nav/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import ContentNavMenu from '../../ContentNavMenu'
 import { getLocationHash } from '../../../utils/window'
 import './index.css'
 
 const AboutNavItem = ({ text, href = '#', id }) => {
   const isActive = getLocationHash() === href
   return (
-    <div 
+    <div
       key={id}
       className={`about-nav-tab-item ${isActive ? 'about-nav-tab-item-is-active' : ''}`}
     >
@@ -16,47 +17,60 @@ const AboutNavItem = ({ text, href = '#', id }) => {
   )
 }
 
-const AboutNav = ({ items }) => (
+const AboutNav2 = ({ items }) => (
     <div className='section-content-only about-nav-wrap'>
       <div className='about-nav-tabs'>
-        {items.map((item, idx) => (
-          <AboutNavItem
-            key={idx}
-            id={idx}
-            text={item.text}
-            href={item.href}
-            isActive={item.isActive}
-          />
-          ))}
+        <div className='about-nav-tabs-inner'>
+          {items.map((item, idx) => (
+            <AboutNavItem
+              key={idx}
+              id={idx}
+              text={item.text}
+              href={item.href}
+              isActive={item.isActive}
+            />
+            ))}
+          </div>
       </div>
     </div>
+)
+
+const AboutNav = ({ items }) => (
+  <div className='section-content-only'>
+    <ContentNavMenu
+      items={items.map(item => ({
+        ...item,
+        className: 'about-content-nav-item'
+      }))}
+    />
+  </div>
 )
 
 AboutNav.defaultProps = {
   items: [{
     text: 'Topic Tables',
-    href: '#topic-tables',
+    href: '/about#topic-tables',
   }, {
     text: 'Ask React Team',
-    href: '#ask-react-team'
+    href: '/about#ask-react-team'
   }, {
     text: 'Unconference',
-    href: '#unconference'
+    href: '/about#unconference'
   }, {
     text: 'Venue and Hotel',
-    href: '#venue-and-hotel'
+    href: '/about#venue-and-hotel'
   }, {
     text: 'Jobs',
-    href: '#jobs'
+    href: '/about#jobs'
   }, {
     text: 'City Guide',
-    href: '#city-guide'
+    href: '/about#city-guide'
   }, {
     text: 'Diversity & Safety',
-    href: '#diversity-and-safety'
+    href: '/about#diversity-and-safety'
   }, {
     text: 'FAQ',
-    href: '#faq'
+    href: '/about#faq'
   }]
 }
 
