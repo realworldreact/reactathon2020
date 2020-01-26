2e2866349dd2bbeb716bf11f9a86cf11fb603dc6
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-01-25 19:51:55 -0800

    Revert "feat(speakers): add speakers page (#38)" (#39)
    This reverts commit af2584fcae01e49e6ef414cd5b8ee844e53a1238.

--- src/components/Home/Speakers/index.js
+++ src/components/Home/Speakers/index.js
@@ -4,19 +4,11 @@ import SpeakerGrid from '../../Speakers/Grid'
 import data from '../../../assets/data/home/speakers.json'
 import IMAGE_MAP from './image-map'
 import './index.css'
-import ViewAllSpeakers from '../../Speakers/ViewAll'
 
-const SpeakersGridWrap = ({ speakers }) => (
-  <div>
-    <SpeakerGrid speakers={speakers} />
-    <br /><br />
-    <ViewAllSpeakers />
-  </div>
-)
 const SpeakerSection = ({ speakers }) => (
   <section className='section-speakers'>
     <SectionTitle text='Speakers' className='title-speakers' />
-    <SpeakersGridWrap speakers={speakers} />
+    <SpeakerGrid speakers={speakers} />
   </section>
 )
 
