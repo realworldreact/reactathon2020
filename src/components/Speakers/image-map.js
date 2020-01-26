48449ba4bedc1e4f7c58f107827cfe48fe0d0325
Prashanth R <prashanthrdev@gmail.com>
2020-01-25 19:44:27 -0800

    feat(speakers): add speakers page

---
+++ src/components/Speakers/image-map.js
@@ -0,0 +1,40 @@
+import RyanFlorenceImg from '../../assets/images/speakers/speaker-ryan.png'
+import JanaBeckImg from '../../assets/images/speakers/speaker-jana.png'
+import CassidyWilliamsImg from '../../assets/images/speakers/speaker-cassidy.png'
+import WesBosImg from '../../assets/images/speakers/speaker-wes.png'
+import GuillermoRauchImg from '../../assets/images/speakers/speaker-guillermo.png'
+import KentCDoddsImg from '../../assets/images/speakers/speaker-kent.png'
+import TejasKumarImg from '../../assets/images/speakers/speaker-tejas.png'
+import MuhsinAbdulMusawwirImg from '../../assets/images/speakers/speaker-muhsin.png'
+import JJKasperImg from '../../assets/images/speakers/speaker-jj.png'
+import SwizecTellerImg from '../../assets/images/speakers/speaker-swizec.png'
+import LaurenTanImg from '../../assets/images/speakers/speaker-lauren.png'
+import BrianLerouxImg from '../../assets/images/speakers/speaker-brian.png'
+import BenAwadImg from '../../assets/images/speakers/speaker-ben.png'
+import RyanBurgessImg from '../../assets/images/speakers/speaker-ryan-burgess.png'
+import TanmaiGopalImg from '../../assets/images/speakers/speaker-tanmai.png'
+import EveProcelloImg from '../../assets/images/speakers/speaker-eve.png'
+import LeeByronImg from '../../assets/images/speakers/speaker-lee.png'
+
+const TableManagerImageMap = {
+  'ryan-florence': RyanFlorenceImg,
+  'jana-beck': JanaBeckImg,
+  'cassidy-williams': CassidyWilliamsImg,
+  'wes-bos': WesBosImg,
+  'guillermo-rauch': GuillermoRauchImg,
+  'kent-c.-dodds': KentCDoddsImg,
+  'chris-on-code': '',
+  'tejas-kumar': TejasKumarImg,
+  'muhsin-abdul-musawwir': MuhsinAbdulMusawwirImg,
+  'jj-kasper': JJKasperImg,
+  'swizec-teller': SwizecTellerImg,
+  'lauren-tan': LaurenTanImg,
+  'brian-leroux': BrianLerouxImg,
+  'ben-awad': BenAwadImg,
+  'ryan-burgess': RyanBurgessImg,
+  'tanmai-gopal': TanmaiGopalImg,
+  'eve-porcello': EveProcelloImg,
+  'lee-byron': LeeByronImg
+}
+
+export default TableManagerImageMap
