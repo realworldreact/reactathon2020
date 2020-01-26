4a7a432bf58198486a95e71e832dcc8a8dd90298
Prashanth R <prashanthrdev@gmail.com>
2020-01-26 14:18:11 -0800

    cleanup about nav

--- src/components/About/Nav/index.js
+++ src/components/About/Nav/index.js
@@ -3,38 +3,6 @@ import ContentNavMenu from '../../ContentNavMenu'
 import { getLocationHash } from '../../../utils/window'
 import './index.css'
 
-const AboutNavItem = ({ text, href = '#', id }) => {
-  const isActive = getLocationHash() === href
-  return (
-    <div
-      key={id}
-      className={`about-nav-tab-item ${isActive ? 'about-nav-tab-item-is-active' : ''}`}
-    >
-      <a href={href}>
-        {text}
-      </a>
-    </div>
-  )
-}
-
-const AboutNav2 = ({ items }) => (
-    <div className='section-content-only about-nav-wrap'>
-      <div className='about-nav-tabs'>
-        <div className='about-nav-tabs-inner'>
-          {items.map((item, idx) => (
-            <AboutNavItem
-              key={idx}
-              id={idx}
-              text={item.text}
-              href={item.href}
-              isActive={item.isActive}
-            />
-            ))}
-          </div>
-      </div>
-    </div>
-)
-
 const AboutNav = ({ items }) => (
   <div className='section-content-only'>
     <ContentNavMenu
