93b5dd5a5ebe118214214da4e3453e48a29b2c9c
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-01-25 19:52:02 -0800

    Revert "Revert "feat(speakers): add speakers page (#38)" (#39)"
    This reverts commit 2e2866349dd2bbeb716bf11f9a86cf11fb603dc6.

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './index.css'
 
 const Banner = ({ className = '', content }) => (
-  <div className={`banner ${className}`}>
+  <div className={`banner banner-content ${className}`}>
     {content}
   </div>
 )
