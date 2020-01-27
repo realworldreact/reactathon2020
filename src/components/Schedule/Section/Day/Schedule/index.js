430a19b2bbcebcfa48a01414291622d42b0029a9
Prashanth R <prashanthrdev@gmail.com>
2020-01-26 23:50:54 -0800

    full schedule layout

--- src/components/Schedule/Section/Day/Schedule/index.js
+++ src/components/Schedule/Section/Day/Schedule/index.js
@@ -1,12 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CaretUpIcon from '../../../../../assets/images/Schedule/show-bio-carrot.svg'
 import './index.css'
+
+const DayScheduleSpeaker = ({ speaker, company, talkTitle, talkDescription, photo, speakerBio, expandBio }) => {
+  const [isBioExpanded, toggleBio] = useState(false)
+  return (
+    <div className='schedule-day-schedule-item-details-speaker-grid'>
+      <div>
+        <img className='schedule-day-schedule-item-details-speaker-img' src={photo} alt={speaker} />
+      </div>
+      <div className='schedule-day-schedule-item-details-speaker-details'>
+        <h2 className='speaker-name'>
+          {speaker}
+        </h2>
+        <span className='speaker-company'>
+          {company}
+        </span>
+        <br />
+        <span
+          className={`schedule-day-schedule-item-details-speaker-grid-bio schedule-day-schedule-item-details-speaker-grid-bio-${isBioExpanded ? 'show' : 'hide'} `}
+        >
+          {speakerBio}
+        </span>
+        <button
+          className='schedule-day-schedule-item-details-speaker-bio-button'
+          onClick={() => toggleBio(!isBioExpanded)}
+        >
+          {isBioExpanded ? 'Hide' : 'Show'} Bio <img src={CaretUpIcon} alt='caret-up' />
+        </button>
+        <br />
+      </div>
+    </div>
+  )
+}
 
 const DayScheduleItem = ({ time, speaker, company, talkTitle, talkDescription, photo, speakerBio, expandBio }) => {
   console.log(time, speaker, company, talkTitle, talkDescription, photo, speakerBio, expandBio)
   return (
     <div className='schedule-day-schedule-item'>
-      {speaker}
-      <hr className='section-divider' />
+      <div className='schedule-day-schedule-item-time'>
+        {time}
+      </div>
+      <div className='schedule-day-schedule-item-details'>
+          <div className='schedule-day-schedule-item-details-title'>
+            {talkTitle}
+          </div>
+          {speaker && (
+            <DayScheduleSpeaker
+              speaker={speaker}
+              company={company}
+              talkTitle={talkTitle}
+              talkDescription={talkDescription}
+              photo={photo}
+              speakerBio={speakerBio}
+              expandBio={expandBio}
+            />
+          )}
+          <div className='schedule-day-schedule-item-details-description'>
+            {talkDescription}
+          </div>
+      </div>
     </div>
   )
 }
@@ -14,17 +67,20 @@ const DayScheduleItem = ({ time, speaker, company, talkTitle, talkDescription, p
 const DaySchedule = ({ schedule }) => (
   <div className='schedule-day-schedule'>
     {schedule.map((scheduleItem, idx) => (
-      <DayScheduleItem
-        key={idx}
-        time={scheduleItem.time}
-        speaker={scheduleItem.speaker}
-        company={scheduleItem.company}
-        talkTitle={scheduleItem.talkTitle}
-        talkDescription={scheduleItem.talkDescription}
-        photo={scheduleItem.photo}
-        speakerBio={scheduleItem.speakerBio}
-        expandBio={scheduleItem.showBioExpands}
-      />
+      <div>
+        <DayScheduleItem
+          key={idx}
+          time={scheduleItem.time}
+          speaker={scheduleItem.speaker}
+          company={scheduleItem.company}
+          talkTitle={scheduleItem.talkTitle}
+          talkDescription={scheduleItem.talkDescription}
+          photo={scheduleItem.photo}
+          speakerBio={scheduleItem.speakerBio}
+          expandBio={scheduleItem.showBioExpands}
+        />
+        <hr className='section-divider' />
+      </div>
     ))}
   </div>
 )
