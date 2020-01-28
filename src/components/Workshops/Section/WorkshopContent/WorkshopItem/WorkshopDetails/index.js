41a114b2be12bba14f7584c55ac4972e345ffdc3
Prashanth R <prashanthrdev@gmail.com>
2020-01-27 22:53:46 -0800

    finish workshops

--- src/components/Workshops/Section/WorkshopContent/WorkshopItem/WorkshopDetails/index.js
+++ src/components/Workshops/Section/WorkshopContent/WorkshopItem/WorkshopDetails/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import WorkshopDetailItemHeader from './Header'
 import WorkshopDetailItemSubHeader from './SubHeader'
+import WorkshopDetailContent from './Content'
 import './index.css'
 
-const WorkshopDetails = ({ headline, time, level, length }) => (
+const WorkshopDetails = ({
+  headline, time, level, length,
+  paragraphOne, bulletsOne, paragraphTwo, bulletsTwo,
+  paragraphThree
+}) => (
   <div className='workshop-item-details'>
     <WorkshopDetailItemHeader title={headline} time={time} />
     <WorkshopDetailItemSubHeader level={level} length={length} />
+    <WorkshopDetailContent
+      paragraphOne={paragraphOne}
+      paragraphTwo={paragraphTwo}
+      paragraphThree={paragraphThree}
+      bulletsOne={bulletsOne}
+      bulletsTwo={bulletsTwo}
+    />
   </div>
 )
 
