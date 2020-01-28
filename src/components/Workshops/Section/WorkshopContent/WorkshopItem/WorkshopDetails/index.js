de02dfd40181327d2d21ff358b58db06c4fc15bc
Prashanth R <prashanthrdev@gmail.com>
2020-01-27 22:05:27 -0800

    get header done

--- src/components/Workshops/Section/WorkshopContent/WorkshopItem/WorkshopDetails/index.js
+++ src/components/Workshops/Section/WorkshopContent/WorkshopItem/WorkshopDetails/index.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import './index.css'
 
-const WorkshopDetails = ({ headline }) => (
+const WorkshopDetailTitle = ({ text }) => (
+  <div className='workshop-item-details-title'>
+     {text}
+  </div>
+)
+
+const WorkshopDetailItemHeader = ({ title, time }) => {
+  const hour = time ? time.substring(0, time.length - 2) : ''
+  const tod = time ? (time.indexOf('am') !== -1 ? 'am' : 'pm') : ''
+
+  return (
+    <div className='workshop-item-details-header'>
+      <WorkshopDetailTime time={time} tod={tod} />
+      <WorkshopDetailTitle text={title} />
+    </div>
+  )
+}
+
+const WorkshopDetailTime = ({ hour, timeOfDay }) => (
+  <div className='workshop-item-details-time'>
+    <div className='workshop-item-details-time-hour'>
+      {hour || '9:00'}
+    </div>
+    <div className='workshop-item-details-time-tod'>
+      {timeOfDay || 'am'}
+    </div>
+  </div>
+)
+
+const WorkshopDetails = ({ headline, time }) => (
   <div className='workshop-item-details'>
-    WorkshopDetails
+    <WorkshopDetailItemHeader title={headline} time={time} />
   </div>
 )
 
