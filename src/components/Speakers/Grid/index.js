9e48a1cb10348b462e8a61ca8307549d592ca01b
Prashanth R <prashanthrdev@gmail.com>
2020-01-29 01:06:01 -0800

    add speaker links in speakers page

--- src/components/Speakers/Grid/index.js
+++ src/components/Speakers/Grid/index.js
@@ -1,17 +1,28 @@
 import React from 'react'
+import Link from '../../Link'
+import { getSpeakerId } from '../../../utils/speaker'
 import IconGithub from '../../../assets/icons/home/icon-github.svg'
 import IconTwitter from '../../../assets/icons/home/icon-twitter.svg'
 import './index.css'
 
+const getSpeakerProfileUrl = (name) => `/speakers/profile#${getSpeakerId(name)}`
+
 const Speaker = ({ id, className = '', imgSrc, imgAlt, name, headline, company, twitterUrl = '#', githubUrl = '#' }) => (
   <div key={id} className={`speaker ${className}`}>
     <div className='speaker-img-wrap'>
-      <img className='speaker-img' src={imgSrc} alt={imgAlt} />
+      {/* <img className='speaker-img' src={imgSrc} alt={imgAlt} /> */}
+      <Link
+        href={getSpeakerProfileUrl(name)}
+        text={<img className='speaker-img' src={imgSrc} alt={imgAlt} />}
+        isExternal={false}
+      />
     </div>
     <div className='speaker-description'>
-      <h2 className='speaker-name'>
-        {name}
-      </h2>
+      <Link
+        href={getSpeakerProfileUrl(name)}
+        text={<h2 className='speaker-name'>{name}</h2>}
+        isExternal={false}
+      />
       <span className='speaker-headline'>
         {headline}
       </span>
