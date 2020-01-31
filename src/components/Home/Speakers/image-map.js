ade52384ded486e0d83080489190c18a469a22f3
Benjamin Dunphy <dunphy.ben@gmail.com>
2020-01-31 14:05:40 -0800

    Replace all speaker PNGs (except profile headers) with JPGs

--- src/components/Home/Speakers/image-map.js
+++ src/components/Home/Speakers/image-map.js
@@ -1,9 +1,9 @@
-import RyanFlorenceImg from '../../../assets/images/home/speakers/speaker-ryan.png'
-import JanaBeckImg from '../../../assets/images/home/speakers/speaker-jana.png'
-import CassidyWilliamsImg from '../../../assets/images/home/speakers/speaker-cassidy.png'
-import WesBosImg from '../../../assets/images/home/speakers/speaker-wes.png'
+import RyanFlorenceImg from '../../../assets/images/home/speakers/speaker-ryan.jpg'
+import JanaBeckImg from '../../../assets/images/home/speakers/speaker-jana.jpg'
+import CassidyWilliamsImg from '../../../assets/images/home/speakers/speaker-cassidy.jpg'
+import WesBosImg from '../../../assets/images/home/speakers/speaker-wes.jpg'
 import GuillermoRauchImg from '../../../assets/images/home/speakers/speaker-guillermo.jpg'
-import KentCDoddsImg from '../../../assets/images/home/speakers/speaker-kent.png'
+import KentCDoddsImg from '../../../assets/images/home/speakers/speaker-kent.jpg'
 
 const SpeakerImageMap = {
   'ryan-florence': RyanFlorenceImg,
