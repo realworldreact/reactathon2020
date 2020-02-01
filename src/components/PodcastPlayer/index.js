e08287afb507f1b97dc6901e9b11a6bd9d98bb7a
Prashanth R <prashanthrdev@gmail.com>
2020-02-01 10:21:00 -0800

    wip

--- src/components/PodcastPlayer/index.js
+++ src/components/PodcastPlayer/index.js
@@ -2,10 +2,15 @@ import React from 'react'
 import Audio from '../Audio'
 import './index.css'
 
-const PodcastPlayer = ({ }) => (
+const PodcastPlayer = ({ src }) => (
   <Audio
-
+    sources={[src]}
+    type={''}
   />
 )
+
+PodcastPlayer.defaultProps = {
+  src: 'https://audio.simplecast.com/63a2d5ab.mp3'
+}
 
 export default PodcastPlayer
