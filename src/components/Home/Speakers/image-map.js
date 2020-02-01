47e751c5a93ce53a2ad661cf00927e451d778f4d
Prashanth R <prashanthrdev@gmail.com>
2020-02-01 12:29:03 -0800

    fix path for wes

--- src/components/Home/Speakers/image-map.js
+++ src/components/Home/Speakers/image-map.js
@@ -1,7 +1,7 @@
 import RyanFlorenceImg from '../../../assets/images/Speakers/speaker-ryan.jpg'
 import JanaBeckImg from '../../../assets/images/Speakers/speaker-jana.jpg'
 import CassidyWilliamsImg from '../../../assets/images/Speakers/speaker-cassidy.jpg'
-import WesBosImg from '../../../assets/images/speakers/Speaker-wes.jpg'
+import WesBosImg from '../../../assets/images/Speakers/speaker-wes.jpg'
 import GuillermoRauchImg from '../../../assets/images/Speakers/speaker-guillermo.jpg'
 import KentCDoddsImg from '../../../assets/images/Speakers/speaker-kent.jpg'
 
