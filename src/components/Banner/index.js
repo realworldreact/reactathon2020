fed5b3e778aa36868305539bff44b7d8d4028704
Prashanth R <prashanthrdev@gmail.com>
2020-02-02 02:43:10 -0800

    try agn

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -4,7 +4,7 @@ import { getScreenWidth } from '../../utils/window'
 import { VIDEO_LOAD_BREAKPOINT } from '../../constants'
 import './index.css'
 
-const Banner = ({ className = '', content, video }) => {
+const Banner = ({ bannerClassName = '', content, video }) => {
   const shouldLoadVideo = getScreenWidth() > VIDEO_LOAD_BREAKPOINT
   return (
     video && shouldLoadVideo
@@ -30,7 +30,7 @@ const Banner = ({ className = '', content, video }) => {
       </div>
     )
     : (
-      <div className={`banner banner-content ${className}`}>
+      <div className={`banner banner-content ${bannerClassName}`}>
         {content}
       </div>
     )
