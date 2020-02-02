35cc1434b1333b06e9e9419e350842355830422d
Prashanth R <prashanthrdev@gmail.com>
2020-02-01 19:38:06 -0800

    highlights wip

---
+++ src/components/Home/Highlights/image-map.js
@@ -0,0 +1,33 @@
+import Image1 from '../../../assets/images/home/highlights/highlights-1.png'
+import Image2 from '../../../assets/images/home/highlights/highlights-2.png'
+import Image3 from '../../../assets/images/home/highlights/highlights-3.png'
+import Image4 from '../../../assets/images/home/highlights/highlights-4.png'
+import Image5 from '../../../assets/images/home/highlights/highlights-5.png'
+import Image6 from '../../../assets/images/home/highlights/highlights-6.png'
+import Image7 from '../../../assets/images/home/highlights/highlights-7.png'
+import Image8 from '../../../assets/images/home/highlights/highlights-8.png'
+import Image9 from '../../../assets/images/home/highlights/highlights-9.png'
+import Image10 from '../../../assets/images/home/highlights/highlights-10.png'
+import Image11 from '../../../assets/images/home/highlights/highlights-11.png'
+import Image12 from '../../../assets/images/home/highlights/highlights-12.png'
+import Image13 from '../../../assets/images/home/highlights/highlights-13.png'
+import Image14 from '../../../assets/images/home/highlights/highlights-14.png'
+
+const HighlightsImages = [
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+  Image6,
+  Image7,
+  Image8,
+  Image9,
+  Image10,
+  Image11,
+  Image12,
+  Image13,
+  Image14
+]
+
+export default HighlightsImages
--- src/components/Home/Highlights/index.js
+++ src/components/Home/Highlights/index.js
@@ -1,35 +1,41 @@
 import React from 'react'
 import SectionTitle from '../SectionTitle'
+import HighlightImages from './image-map'
 import './index.css'
 
-const HighlightGalleryImage = ({ id, imgSrc, imgAlt, className = '' }) => (
+const HighlightGalleryImage = ({ id, imgSrc, imgAlt, className = '', isClickable = false, videoSrc }) => (
   <img
     key={id}
     src={imgSrc}
     alt={imgAlt}
     className={className}
+    onClick={() => {}}
   />
 )
 
 const HighlightGallery = ({ images }) => (
   <div className='highlight-gallery'>
     {images.map((image,idx) => (
-      <HighlightGalleryImage 
+      <HighlightGalleryImage
         key={idx}
         id={idx}
         className='highlight-gallery-image'
         imgSrc={image.src}
         imgAlt={image.alt}
-      /> 
+        videoSrc={image.videoSrc}
+        isClickable={image.isClickable}
+      />
     ))}
   </div>
 )
 
 HighlightGallery.defaultProps = {
-  images: [{
-    src: '',
-    alt: ''
-  }]
+  images: HighlightImages.map((img, idx) => ({
+    src: img,
+    alt: `highlight-img-${idx}`,
+    videoSrc: '',
+    isClickable: idx === HighlightImages.length - 1
+  }))
 }
 
 const Highlights = () => (
