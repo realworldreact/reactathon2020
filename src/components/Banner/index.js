f6f960f4b9738eee075040897f0b3acecf0cdf29
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-02-02 01:28:15 -0800

    feat(header-video-images): Add header videos and update images (#90)
    * Headers/Add main route header images
    
    * handle banner video on mobile
    
    * add all header images
    
    * feature css
    
    Co-authored-by: Benjamin Dunphy <dunphy.ben@gmail.com>

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import Video from '../Video'
+import { getScreenWidth } from '../../utils/window'
+import { VIDEO_LOAD_BREAKPOINT } from '../../constants'
 import './index.css'
 
-const Banner = ({ className = '', content }) => (
-  <div className={`banner banner-content ${className}`}>
-    {content}
-  </div>
-)
+const Banner = ({ className = '', content, video }) => {
+  const shouldLoadVideo = getScreenWidth() > VIDEO_LOAD_BREAKPOINT
+  return (
+    video && shouldLoadVideo
+    ? (
+      <div className='banner-wrap'>
+        {video && (
+          <div className='banner-video-wrap'>
+            <Video
+              className='banner-video'
+              loop={true}
+              autoPlay
+              muted
+              showControls={false}
+              src={video && (shouldLoadVideo && video.src)}
+              poster={video && (shouldLoadVideo ? video.poster : video.mobilePoster)}
+              isExternalSource={false}
+            />
+          </div>
+        )}
+        <div className='banner-content-overlay'>
+          {content}
+        </div>
+      </div>
+    )
+    : (
+      <div className={`banner banner-content ${className}`}>
+      {content}
+      </div>
+    )
+  )
+}
 
 export default Banner
