0ece78a95261ca6e26c773bb20866c98f5aba86f
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-02-01 23:54:37 -0800

    feat(home-highlights): Add home highlights (#88)
    * highlights wip
    
    * wip
    
    * integrate modal
    
    * add video
    
    * resize highlights on mobile
    
    * finish highlights
    
    * finish highlights

--- src/components/Home/Highlights/index.js
+++ src/components/Home/Highlights/index.js
@@ -1,35 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SectionTitle from '../SectionTitle'
+import Video from '../../Video'
+import Modal from '../../Modal'
+import HighlightImages from './image-map'
 import './index.css'
 
-const HighlightGalleryImage = ({ id, imgSrc, imgAlt, className = '' }) => (
-  <img
-    key={id}
-    src={imgSrc}
-    alt={imgAlt}
-    className={className}
+const HighlightGalleryImage = ({ id, imgSrc, imgAlt, className = '', onClick, isClickable = false }) => (
+    <div
+      role='button'
+      tabIndex={0}
+      className={`${isClickable ? 'highlight-img-video' : ''}`}
+      onClick={() => {
+        if (isClickable) {
+          onClick()
+        }
+      }}
+      onKeyPress={() => {}}
+    >
+      <img
+        key={id}
+        src={imgSrc}
+        alt={imgAlt}
+        className={className}
+      />
+    </div>
+)
+
+const HighlightVideo = ({ src, toggleModalVisibility, isModalOpen }) => (
+  <Modal
+    className='highlight-video-modal'
+    body={(
+      <Video
+        src={src}
+        className='highlight-video-frame'
+        allowFullScreen={false}
+        isExternalSource={true}
+      />
+    )}
+    isModalOpen={isModalOpen}
+    onClose={() => toggleModalVisibility(!isModalOpen)}
   />
 )
 
-const HighlightGallery = ({ images }) => (
-  <div className='highlight-gallery'>
-    {images.map((image,idx) => (
-      <HighlightGalleryImage 
-        key={idx}
-        id={idx}
-        className='highlight-gallery-image'
-        imgSrc={image.src}
-        imgAlt={image.alt}
-      /> 
-    ))}
-  </div>
-)
+const HighlightGallery = ({ images }) => {
+  const [isModalOpen, toggleModalVisibility] = useState(false)
+  const [videoSrc, setVideoSrc] = useState(null)
+  return (
+    <div className='highlight-gallery'>
+      {images.map((image,idx) => (
+        <HighlightGalleryImage
+          key={idx}
+          id={idx}
+          className='highlight-gallery-image'
+          imgSrc={image.src}
+          imgAlt={image.alt}
+          isClickable={image.isClickable}
+          onClick={() => {
+            if (image.isClickable) {
+              setVideoSrc(image.videoSrc)
+              toggleModalVisibility(!isModalOpen)
+            }
+          }}
+        />
+      ))}
+      <HighlightVideo
+        src={videoSrc}
+        isModalOpen={isModalOpen}
+        toggleModalVisibility={toggleModalVisibility}
+      />
+    </div>
+  )
+}
 
 HighlightGallery.defaultProps = {
-  images: [{
-    src: '',
-    alt: ''
-  }]
+  images: HighlightImages.map((img, idx) => ({
+    src: img.src,
+    alt: img.alt || `highlight-img-${idx}`,
+    videoSrc: img.videoSrc,
+    isClickable: !!img.videoSrc
+  }))
 }
 
 const Highlights = () => (
