939f275ffe838e9f7722418bbad189bd236c0499
Prashanth R <prashanthrdev@gmail.com>
2020-02-02 23:51:32 -0800

    add html formatting from json text

--- src/components/About/Section/Jobs/hiring-mixer.js
+++ src/components/About/Section/Jobs/hiring-mixer.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import SubPageSectionHeader from '../../../SubPageSectionHeader'
 import AboutSectionDateTime from '../AboutSectionDateTime'
+import TextWithHtml from '../../../TextWithHtml'
 import LineButton from '../../../LineButton'
 
-const HiringMixer = ({ title, date, time, subtext, buttonText, buttonHref }) => (
+const HiringMixer = ({ title, date, time, subtext, contactInfo, buttonText, buttonHref }) => (
   <div className='section-jobs-hiring-mixer'>
     <SubPageSectionHeader text={title} className='section-jobs-header' />
     <AboutSectionDateTime date={date} time={time} />
-    <p>{subtext}</p>
+    <p>
+      <TextWithHtml
+        placeholder={contactInfo.placeholder}
+        replaceLink={contactInfo.href}
+        replaceText={contactInfo.text}
+        text={subtext}
+      />
+    </p>
     <LineButton
       className='sections-jobs-hiring-mixer-cta-btn'
       href={buttonHref}
