6a50ae404a3eaf36c26411776e52eab6b023e0b0
Prashanth R <prashanthrdev@gmail.com>
2020-02-02 22:50:56 -0800

    chore(website-globe): Add website globe

--- src/utils/speaker.js
+++ src/utils/speaker.js
@@ -4,3 +4,5 @@ export const getSpeakerId = (name) => {
 }
 
 export const getSpeakerProfileUrl = (name) => `/speakers/profile#${getSpeakerId(name)}`
+
+export const sanitizeWebsiteUrl = (url) => url.replace('http://', '').replace('https://', '').replace('www.', '')
