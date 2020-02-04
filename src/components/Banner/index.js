432a2ff9f3548048d438c0fe078848f6ab80b26c
Swizec Teller <swizec@swizec.com>
2020-02-04 07:44:42 -0800

    Autoplay video in header

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,40 +1,34 @@
-import React from 'react'
-import Video from '../Video'
-import { getScreenWidth } from '../../utils/window'
-import { NATIVE_BREAKPOINT } from '../../constants'
-import './index.css'
+import React from "react";
+import Video from "../Video";
+import { getScreenWidth } from "../../utils/window";
+import { NATIVE_BREAKPOINT } from "../../constants";
+import "./index.css";
+import homeVideo from "../../assets/videos/home-header-video.mp4";
 
-const Banner = ({ className = '', content, video }) => {
-  const shouldLoadVideo = getScreenWidth() > NATIVE_BREAKPOINT
-  return (
-    video && shouldLoadVideo
-    ? (
-      <div className='banner-wrap'>
-        {video && (
-          <div className='banner-video-wrap'>
-            <Video
-              className='banner-video'
-              loop={true}
-              autoPlay
-              muted
-              showControls={false}
-              src={video && (shouldLoadVideo && video.src)}
-              poster={video && (shouldLoadVideo ? video.poster : video.mobilePoster)}
-              isExternalSource={false}
-            />
-          </div>
-        )}
-        <div className='banner-content-overlay'>
-          {content}
-        </div>
-      </div>
-    )
-    : (
-      <div className={`banner banner-content ${className}`}>
-      {content}
-      </div>
-    )
-  )
-}
+const Banner = ({ className = "", content, video = { src: homeVideo } }) => {
+  const shouldLoadVideo = getScreenWidth() > NATIVE_BREAKPOINT;
+  console.log(video, shouldLoadVideo);
+  return video && shouldLoadVideo ? (
+    <div className="banner">
+      {video && (
+        <Video
+          className="banner-video"
+          loop={true}
+          autoPlay
+          muted
+          showControls={false}
+          src={video && shouldLoadVideo && video.src}
+          poster={
+            video && (shouldLoadVideo ? video.poster : video.mobilePoster)
+          }
+          isExternalSource={false}
+        />
+      )}
+      <div className="banner-content-overlay">{content}</div>
+    </div>
+  ) : (
+    <div className={`banner banner-content ${className}`}>{content}</div>
+  );
+};
 
-export default Banner
+export default Banner;
--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,7 +1,16 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+import "./index.css";
 
-const InternalVideoPlayer = ({ loop = false, src, sources = [], tracks = [{ kind: 'captions' }], showControls = true, className = '', autoPlay, muted }) => (
+const InternalVideoPlayer = ({
+  loop = false,
+  src,
+  sources = [],
+  tracks = [{ kind: "captions" }],
+  showControls = true,
+  className = "",
+  autoPlay,
+  muted
+}) => (
   <video
     autoPlay={autoPlay}
     muted={muted}
@@ -10,45 +19,72 @@ const InternalVideoPlayer = ({ loop = false, src, sources = [], tracks = [{ kind
     loop={loop}
     src={src}
   >
-    <track kind='captions' />
+    <track kind="captions" />
     {sources.map((source, idx) => (
-      <source
-        key={idx}
-        src={source.src}
-        type={source.type}
-      />
+      <source key={idx} src={source.src} type={source.type} />
     ))}
     {tracks.map((track, idx) => (
       <track
         key={idx}
         src={track.src}
         kind={track.kind}
-        srclang={track.srclang}
+        srcLang={track.srclang}
         label={track.label}
         default={track.isDefault}
       />
     ))}
   </video>
-)
+);
 
-const ExternalVideoPlayer = ({ className, width, height, src, title, allow = 'accelerometer;encrypted-media;gyroscope;picture-in-picture', allowFullScreen = true }) => (
-  <div className='ext-video-player'>
+const ExternalVideoPlayer = ({
+  className,
+  width,
+  height,
+  src,
+  title,
+  allow = "accelerometer;encrypted-media;gyroscope;picture-in-picture",
+  allowFullScreen = true
+}) => (
+  <div className="ext-video-player">
     <iframe
       title={title}
       className={className}
       src={src}
       allow={allow}
       allowFullScreen={allowFullScreen}
-      width={width || ''}
-      height={height || ''}
+      width={width || ""}
+      height={height || ""}
     />
   </div>
-)
+);
 
-const Video = ({ loop = false, autoPlay = false, muted = false, title = '', isExternalSource = false, sources, tracks, src, showControls = true, className = '', allowFullScreen = true }) => (
-  isExternalSource
-  ? <ExternalVideoPlayer src={src} className={className} title={title} />
-  : <InternalVideoPlayer loop={loop} autoPlay={autoPlay} muted={muted} src={src} sources={sources} tracks={tracks} showControls={showControls} className={className} allowFullScreen={allowFullScreen} />
-)
+const Video = ({
+  loop = false,
+  autoPlay = false,
+  muted = false,
+  title = "",
+  isExternalSource = false,
+  sources,
+  tracks,
+  src,
+  showControls = true,
+  className = "",
+  allowFullScreen = true
+}) =>
+  isExternalSource ? (
+    <ExternalVideoPlayer src={src} className={className} title={title} />
+  ) : (
+    <InternalVideoPlayer
+      loop={loop}
+      autoPlay={autoPlay}
+      muted={muted}
+      src={src}
+      sources={sources}
+      tracks={tracks}
+      showControls={showControls}
+      className={className}
+      allowFullScreen={allowFullScreen}
+    />
+  );
 
-export default Video
+export default Video;
