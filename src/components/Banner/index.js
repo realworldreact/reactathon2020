c4f2ee92346a9d3ed0c30e94178423e6f1fd0a54
Swizec Teller <swizec@swizec.com>
2020-02-04 08:06:18 -0800

    Add video overlay and ensure only homepage has video

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,28 +3,29 @@ import Video from "../Video";
 import { getScreenWidth } from "../../utils/window";
 import { NATIVE_BREAKPOINT } from "../../constants";
 import "./index.css";
-import homeVideo from "../../assets/videos/home-header-video.mp4";
 
-const Banner = ({ className = "", content, video = { src: homeVideo } }) => {
+const Banner = ({ className = "", content, video }) => {
   const shouldLoadVideo = getScreenWidth() > NATIVE_BREAKPOINT;
-  console.log(video, shouldLoadVideo);
   return video && shouldLoadVideo ? (
     <div className="banner">
-      {video && (
-        <Video
-          className="banner-video"
-          loop={true}
-          autoPlay
-          muted
-          showControls={false}
-          src={video && shouldLoadVideo && video.src}
-          poster={
-            video && (shouldLoadVideo ? video.poster : video.mobilePoster)
-          }
-          isExternalSource={false}
-        />
-      )}
-      <div className="banner-content-overlay">{content}</div>
+      <div className="banner-video-wrap">
+        <div className="banner-video-overlay"></div>
+        {video && (
+          <Video
+            className="banner-video"
+            loop={true}
+            autoPlay
+            muted
+            showControls={false}
+            src={video && shouldLoadVideo && video.src}
+            poster={
+              video && (shouldLoadVideo ? video.poster : video.mobilePoster)
+            }
+            isExternalSource={false}
+          />
+        )}
+        <div className="banner-content-overlay">{content}</div>
+      </div>
     </div>
   ) : (
     <div className={`banner banner-content ${className}`}>{content}</div>
