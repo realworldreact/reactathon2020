324025ebc18d75e6cbf1de45ed9319fe1d540855
Prashanth R <prashanthrdev@gmail.com>
2020-02-09 23:45:57 -0800

    feat(netlify-form): Fix up form

--- src/components/MainContentWrapper/Footer/Newsletter/index.js
+++ src/components/MainContentWrapper/Footer/Newsletter/index.js
@@ -1,26 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.css'
 
-const Newsletter = () => (
-  <div className='footer-newsletter'>
-    <div className="footer-newsletter-wrap">
-      <em>Stay up-to-date with our (non-spammy) newsletter.</em>
-      <form id="newsletter-form" name="Newsletter" method="POST">
-          <input type='hidden' name='form-name' value='Newsletter' />
-          <input type='hidden' className='hidden' name='bot-field' />
-          <input type='text' name='email' placeholder='Fixing this brb...' className='email' />
-          <button type='submit' className='subscribe'>
-            Subscribe
-          </button>
-      </form>
+const Newsletter = () => {
+  return (
+    <div className='footer-newsletter'>
+      <div className='footer-newsletter-wrap'>
+        <em>Stay up-to-date with our (non-spammy) newsletter.</em>
+        <form id='newsletter-form' name='Newsletter' method='POST' data-netlify='true' netlify-honeypot='bot-field' action='/newsletter'>
+            <input type='hidden' name='form-name' value='Newsletter' />
+            <input type='hidden' className='hidden' name='bot-field' />
+            <input type='text' name='email' placeholder='Fixing this brb...' className='email' />
+            <button type='submit' className='subscribe'>
+              Subscribe
+            </button>
+        </form>
+      </div>
     </div>
-    {/* <div class="thank-you message">
-      <em>Thank you! We'll keep you up-to-date with the latest info.</em>
-    </div>
-    <div class="failed message">
-      <em>Uh oh! There was an error. Please try again.</em>
-    </div> */}
-  </div>
-)
+  )
+}
 
 export default Newsletter
