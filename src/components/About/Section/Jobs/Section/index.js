4cd52395b1c0b4e502ed20f135a872f8f8f83a3a
Prashanth R <prashanthrdev@gmail.com>
2020-02-09 21:43:47 -0800

    add job list skeleton

--- src/components/About/Section/Jobs/Section/index.js
+++ src/components/About/Section/Jobs/Section/index.js
@@ -1,20 +1,55 @@
 import React from 'react'
+import LineButton from '../../../../LineButton'
 import SubPageSectionHeader from '../../../../SubPageSectionHeader'
 import './index.css'
 
-const Job = ({ company = '', jobTitle, jobDescription }) => (
-  company
+const JobLocation = ({ location, remote }) => (
+  <div className='job-location-wrap'>
+    <span className='job-location'>
+      <img src='' />
+      {location}
+    </span> &nbsp;
+    {remote && (
+      <span className='job-location-remote'>
+        Remote Ok
+      </span>
+    )}
+  </div>
 )
 
-const JobSection = ({ title, jobs = [], noJobsSubtext }) => (
-  <div className='section-jobs-section'>
-    <SubPageSectionHeader
-      text={title}
-      className='section-jobs-section-title'
-    />
+const Job = ({ company = '', title, description, location, remote = false, logo, link  }) => (
+  <div className='job-item-grid'>
+    <div className='job-item-grid-left'>
+      <div className='job-logo'>
+        <img src={logo} alt={`${company}`} />
+      </div>
+    </div>
+    <div className='job-item-grid-right'>
+      <h2 className='job-company'>
+        {company}
+      </h2>
+      <p className='job-title'>
+        {title}
+      </p>
+      <JobLocation location={location} remote={remote} />
+      <p className='job-description'>
+        {description}
+      </p>
+      <LineButton
+        className='job-cta'
+        href={link}
+        text='See Full Job Description'
+        isExternalLink={true}
+      />
+    </div>
+  </div>
+)
+
+const JobGrid = ({ jobs, noJobsSubtext }) => (
+  <div className='jobs-grid-wrap'>
     {jobs.length > 0
     ? (
-      <div className='section-jobs-section-job-list'>
+      <div className='jobs-grid'>
         {jobs.map((job, idx) => (
           <Job key={idx} {...job} />
         ))}
@@ -26,6 +61,16 @@ const JobSection = ({ title, jobs = [], noJobsSubtext }) => (
       </div>
     )
     }
+  </div>
+)
+
+const JobSection = ({ title, jobs = [], noJobsSubtext }) => (
+  <div className='section-jobs-section'>
+    <SubPageSectionHeader
+      text={title}
+      className='section-jobs-section-title'
+    />
+    <JobGrid jobs={jobs} noJobsSubtext={noJobsSubtext} />
   </div>
 )
 
