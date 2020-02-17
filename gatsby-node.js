164263f74c6a059196dc545f70f0d5cbaf74f0fa
Prashanth R <prashanthrdev@gmail.com>
2020-02-16 20:57:31 -0800

    fix speaker routing

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,22 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// gatsby-node.js
+// Implement the Gatsby API “onCreatePage”.
+// This is called after every page is created.
 
- // You can delete this file if you're not using it
+const pageRouteMap = [{
+  match: /^\/speakers\/profile/
+  matchPath: '/speakers/profile/*'
+}]
+
+exports.onCreatePage = async ({ page, actions }) => {
+  const { createPage } = actions
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  console.log('page', page)
+  if (page.path.match(/^\/speakers\/profile/)) {
+    page.matchPath = "/speakers/profile/*"
+
+    // Update the page.
+    createPage(page)
+  }
+}
