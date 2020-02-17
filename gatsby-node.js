1c70334d8784fb1926ab50140a0c837815d5ef43
Prashanth R <prashanthrdev@gmail.com>
2020-02-16 21:12:33 -0800

    refactor createPage handler

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,10 +2,16 @@
 // Implement the Gatsby API “onCreatePage”.
 // This is called after every page is created.
 
-const pageRouteMap = [{
-  match: /^\/speakers\/profile/
+const speakerProfilePage = {
+  match: /^\/speakers\/profile/,
   matchPath: '/speakers/profile/*'
-}]
+}
+
+const createPageHandler = ({ page, matchPath, createPage }) => {
+  page.matchPath = matchPath
+  // Update the page.
+  createPage(page)
+}
 
 exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions
@@ -13,10 +19,11 @@ exports.onCreatePage = async ({ page, actions }) => {
   // page.matchPath is a special key that's used for matching pages
   // only on the client.
   console.log('page', page)
-  if (page.path.match(/^\/speakers\/profile/)) {
-    page.matchPath = "/speakers/profile/*"
-
-    // Update the page.
-    createPage(page)
+  if (page.path.match(speakerProfilePage.match)) {
+    createPageHandler({
+      createPage,
+      page,
+      matchPath: speakerProfilePage.matchPath
+    })
   }
 }
