1fe03f4cfb93fb4da51d2780a8763196a39ba604
Prashanth R <prashanthrdev@gmail.com>
2020-02-17 22:22:26 -0800

    static routing for wes

--- src/pages/speakers/profile/index.js
+++ src/pages/speakers/profile/index.js
@@ -2,13 +2,10 @@ import React from 'react'
 import LayoutWrapper from '../../../layouts/layout-wrapper'
 import SpeakerProfileComponent from '../../../components/SpeakerProfile'
 
-const SpeakerProfile = (props) => {
-  const speakerId = props['*']
-  return (
-    <LayoutWrapper>
-      <SpeakerProfileComponent id={speakerId} />
-    </LayoutWrapper>
-  )
-}
+const SpeakerProfile = ({ speakerId }) => (
+  <LayoutWrapper>
+    <SpeakerProfileComponent id={speakerId} />
+  </LayoutWrapper>
+)
 
 export default SpeakerProfile
---
+++ src/pages/speakers/profile/wes-bos.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import SpeakerProfile from './index'
+const speakerId = 'wes-bos'
+const Profile = () => (<SpeakerProfile speakerId={speakerId} />)
+export default Profile
