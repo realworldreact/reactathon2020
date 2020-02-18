570c307433932bff8c3f5ad21983f2e4fbdd5eec
Prashanth R <prashanthrdev@gmail.com>
2020-02-17 21:15:35 -0800

    fix invalid path

--- src/utils/404.js
+++ src/utils/404.js
@@ -4,7 +4,7 @@ export const showNotFound = () => (
   window &&
   typeof window.location.pathname === 'string' &&
   (
-    !window.location.pathname.startsWith('/speaker/profile/') ||
+    !window.location.pathname.startsWith('/speakers/profile/') ||
     !window.location.pathname.startsWith('/about/') ||
     !window.location.pathname.startsWith('/schedule/')
   )
