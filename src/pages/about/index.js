1b05a23c832ab1f014eb8525081879f0173cf640
Prashanth R <prashanthrdev@gmail.com>
2020-02-18 19:17:12 -0800

    fix faq routing

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import LayoutWrapper from '../../layouts/layout-wrapper'
 import AboutComponent from '../../components/About'
 
-const About = ({ sectionId }) => (
+const About = ({ sectionId, subRouteId }) => (
   <LayoutWrapper>
-    <AboutComponent sectionId={sectionId} />
+    <AboutComponent sectionId={sectionId} subRouteId={subRouteId} />
   </LayoutWrapper>
 )
 
