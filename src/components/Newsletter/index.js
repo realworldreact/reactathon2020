0b28278b53b4ceb473996974cc6de393d9d7a753
Prashanth R <prashanthrdev@gmail.com>
2020-02-19 19:17:41 -0800

    rename newsletter styles

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -4,21 +4,21 @@ import './index.css'
 
 const Newsletter = () => (
   <div>
-    <section className='section-content section-404'>
-      <h2 className='section-404-header'>
+    <section className='section-content section-newsletter'>
+      <h2 className='section-newsletter-header'>
         Thank you!
       </h2>
-      <p className='section-404-subtext'>
+      <p className='section-newsletter-subtext'>
         We'll keep you up-to-date with the latest info.
       </p>
-      <p className='section-404-subtext'>
+      <p className='section-newsletter-subtext'>
         Need anything else? Perhaps we can help you out. &nbsp;
         <a href="mailto:ben@realworldreact.com">
           Drop us a line
         </a>.
       </p>
     </section>
-    <div className='section-404-bottom'>
+    <div className='section-newsletter-bottom'>
       <Divider />
     </div>
   </div>
