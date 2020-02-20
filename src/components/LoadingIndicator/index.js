28a82adee15fea7c323fa095317c01bfdb981c38
Prashanth R <prashanthrdev@gmail.com>
2020-02-19 18:56:04 -0800

    add 404 timer

---
+++ src/components/LoadingIndicator/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './index.css'
+
+const LoadingIndicator = () => (
+  <span>Loading...</span>
+)
+
+export default LoadingIndicator
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,24 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Divider from '../Divider'
+import LoadingIndicator from '../LoadingIndicator'
 import './index.css'
 
-const NotFound = () => (
-  <div>
-    <section className='section-content section-404'>
-      <h2 className='section-404-header'>
-        404: This page cannot be found.
-      </h2>
-      <p className='section-404-subtext'>
-        Perhaps we can help you out. &nbsp;
-        <a href="mailto:ben@realworldreact.com">
-          Drop us a line
-        </a>.
-      </p>
-    </section>
-    <div className='section-404-bottom'>
-      <Divider />
-    </div>
-  </div>
-)
+const LOADER_TIMEOUT = 3000
+
+const NotFound = () => {
+  const [isLoading, setLoading] = useState(true)
+  useEffect(() => {
+    const loadingTimer = setTimeout(() => {
+      setLoading(false)
+    }, LOADER_TIMEOUT);
+    return () => clearTimeout(loadingTimer);
+  }, [])
+  return (
+    !isLoading
+    ? (
+      <div>
+        <section className='section-content section-404'>
+          <h2 className='section-404-header'>
+            404: This page cannot be found.
+          </h2>
+          <p className='section-404-subtext'>
+            Perhaps we can help you out. &nbsp;
+            <a href="mailto:ben@realworldreact.com">
+              Drop us a line
+            </a>.
+          </p>
+        </section>
+        <div className='section-404-bottom'>
+          <Divider />
+        </div>
+      </div>
+    )
+    : <LoadingIndicator />
+  )
+}
 
 export default NotFound
