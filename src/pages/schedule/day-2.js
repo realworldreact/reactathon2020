d67e7caaf7846a1b6f47773e1e2edc2c146470ab
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-02-19 18:42:25 -0800

    Revert "Revert "WIP - DO NOT MERGE - Revert "Revert "fix(static-routing): Make routing static to avoid 404s on first load""""

---
+++ src/pages/schedule/day-2.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import RootComponent from './index'
+const Component = () => (<RootComponent sectionId={'day-2'} />)
+export default Component
---
+++ src/pages/speakers/profile/swizec-teller.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import RootComponent from './index'
+const Component = () => (<RootComponent speakerId={'swizec-teller'} />)
+export default Component
