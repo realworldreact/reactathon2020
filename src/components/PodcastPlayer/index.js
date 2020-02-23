a74abc74deab9b26133ab70028c0920c904a9556
Prashanth R <prashanthrdev@gmail.com>
2020-02-22 18:40:30 -0800

    fix(progress): Fix seek functionality once and for all

--- src/components/PodcastPlayer/index.js
+++ src/components/PodcastPlayer/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, /*useLayoutEffect*/ } from 'react'
 import Audio from '../Audio'
 import { getScreenWidth } from '../../utils/window'
-import { getUserFriendlyTime, getUpdatedSeekTime, getSeekBoundaries } from '../../utils/audio'
+import { getUserFriendlyTime, getUpdatedSeekTime, getSeekPercentChange } from '../../utils/audio'
 import { WIDE_BREAKPOINT, /*NATIVE_BREAKPOINT*/ } from '../../constants'
 import './index.css'
 
@@ -62,31 +62,27 @@ const PodcastPlayer = ({ className = '', srcFile, track, artist, type = 'mp3', a
     })
   }
 
-  const onVolumeChange = () => {
-    // console.log('onVolumeChange', onVolumeChange)
-    // todo
+  const onVolumeChange = (event, element) => {
+    const percentChange = getSeekPercentChange({ event, element })
+    audioRef.volume = percentChange
+    setPlayerState({
+      ...playerState,
+      volume: percentChange
+    })
   }
 
-  const onProgressSeek = (e, element) => {
-    // console.log('onProgressSeek', playerState)
-    const rect = e.target.getBoundingClientRect()
-    const { clientX } = e
-    const { min, max, move, percent } = getSeekBoundaries({ rect, clientX, element })
-    // const min = rect.left
+  const onProgressSeek = (event, element) => {
+    const percentChange = getSeekPercentChange({ event, element })
 
-    //const max = rect.right || el.offsetWidth
-    //const move = e.clientX
-    //const percent =  (move - rect.x) / max
-
-    // console.log('el', el.position, el.offsetTop, el.offsetLeft, el.offsetWidth, e.clientX)
-    // console.log('min, max, rect, move, percent', min, max, rect, move, percent)
     const newTime = getUpdatedSeekTime({
       currentTime: playerState.currentTime,
       totalTime: playerState.duration,
-      percent: percent
+      percent: percentChange
     })
+
     // Set audio current time
     audioRef.currentTime = newTime
+
     // Update player state
     setPlayerState({
       ...playerState,
@@ -195,12 +191,21 @@ const PodcastDuration = ({ current, total }) => (
   </div>
 )
 
-const PodcastVolumeControl = ({ level, isMuted = false, onToggleMute, onVolumeChange }) => (
-  <div className='podcast-volume'>
-    <button onClick={onToggleMute} className={`podcast-volume-btn ${isMuted ? 'podcast-volume-off-btn' : 'podcast-volume-on-btn'}`} />
-    <progress className='podcast-volume-progress' value={isMuted ? 0 : level} max={1} />
-  </div>
-)
+const PodcastVolumeControl = ({ level, isMuted = false, onToggleMute, onVolumeChange }) => {
+  let volumeProgressRef = null
+  return (
+    <div className='podcast-volume'>
+      <button onClick={onToggleMute} className={`podcast-volume-btn ${isMuted ? 'podcast-volume-off-btn' : 'podcast-volume-on-btn'}`} />
+      <progress
+        className='podcast-volume-progress'
+        ref={el => { volumeProgressRef = el }}
+        onClick={e => onVolumeChange(e, volumeProgressRef)}
+        value={isMuted ? 0 : level}
+        max={1}
+      />
+    </div>
+  )
+}
 
 const PodcastAudio = ({
   audioComponent, track, artist, albumArt,
--- src/utils/audio.js
+++ src/utils/audio.js
@@ -10,21 +10,22 @@ export const getUserFriendlyTime = (durationInSeconds) => {
 
 export const getUpdatedSeekTime = ({ currentTime, totalTime, percent }) => {
   const value = percent * totalTime
-  // console.log('newVal', percent, currentTime, totalTime, value)
   return value
 }
 
-export const getSeekBoundaries = ({ rect, clientX, element }) => {
+export const getSeekPercentChange = ({ event, element }) => {
+  const rect = event.target.getBoundingClientRect()
+
   const { x, left, right } = rect
   const { offsetWidth } = element
-  const min = left // not used?
+  const { clientX } = event
+
+  const min = x || left
   const max = right || offsetWidth
   const move = clientX
-  const percent =  (move - x) / max
-  return {
-    min,
-    max,
-    move,
-    percent
-  }
+
+  const change = (move - min) / (max - min)
+  const percent = change > 1 ? 1 : change
+
+  return percent
 }
