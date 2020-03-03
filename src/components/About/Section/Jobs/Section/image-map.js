57262f6dda5f36c693883dc181653b7708a57657
Benjamin Dunphy <dunphy.ben@gmail.com>
2020-03-02 23:00:13 -0800

    About/Fix jobs margins and add Imgix job posting

--- src/components/About/Section/Jobs/Section/image-map.js
+++ src/components/About/Section/Jobs/Section/image-map.js
@@ -1,9 +1,11 @@
 import FlexportLogo from '../../../../../assets/images/home/sponsors/sponsor-flexport.svg'
 import CourseHeroLogo from '../../../../../assets/images/home/sponsors/sponsor-course-hero.svg'
+import ImgixLogo from '../../../../../assets/images/home/sponsors/sponsor-imgix.svg'
 
 const JobImageMap = {
   'flexport': FlexportLogo,
-  'course-hero': CourseHeroLogo
+  'course-hero': CourseHeroLogo,
+  'imgix': ImgixLogo
 }
 
 export default JobImageMap
