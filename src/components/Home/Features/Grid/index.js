0e9b4ac24808699a55d1244198bb30c37ebab6f2
DmytroPopovSSA <dmytro.popov@ssa-outsourcing.com>
2021-11-14 23:06:25 +0200

    Feature - added new data & added an image (svg)

--- src/components/Home/Features/Grid/index.js
+++ src/components/Home/Features/Grid/index.js
@@ -3,6 +3,7 @@ import LineButton from '../../../LineButton'
 import WelcomeReceptionIcon from '../../../../assets/icons/home/features-icon-reception.svg'
 import TopicTablesIcon from '../../../../assets/icons/home/features-icon-topic-tables.svg'
 import ConferenceTalksIcon from '../../../../assets/icons/home/features-icon-talks.svg'
+import UnconferenceIcon from '../../../../assets/icons/home/features-icon-unconference.svg'
 // import AskReactTeamIcon from '../../../../assets/icons/home/features-icon-ask-react-team.svg'
 import HiringMixerIcon from '../../../../assets/icons/home/features-icon-hiring-mixer.svg'
 import WorkshopsIcon from '../../../../assets/icons/home/features-icon-workshops.svg'
@@ -50,55 +51,59 @@ const FeaturesGrid = ({ features }) => (
 
 FeaturesGrid.defaultProps = {
   features: [{
-    title: 'Daily Mini Workshops',
+    title: 'Welcome Reception',
     description: `
-      Each day kicks off with a number of 2-hour workshops for you to 
-      choose from. Included with the full-access ticket! Click to learn
-      more about the featured workshops. 
-    `,
-    imgSrc: WorkshopsIcon,
-    imgAlt: 'workshops',
-    ctaUrl: '/workshops'
-  }, {
-    title: 'Conference Talks',
-    description: `
-      Three days of talks heavy on content, applicability, and orignality.
-      With over 15 speakers discussing a variety of subjects, you're sure
-      to learn many things to implement back at the office.
-    `,
-    imgSrc: ConferenceTalksIcon,
-    imgAlt: 'conference-talks',
-    ctaUrl: '/schedule'
-  }, {
-    title: 'Sponsor Expo',
-    description: `
-      Our sponsors aren't just incredibly successful businesses -- they're
-      also engineers on the cutting edge of web development. Mingle with the 
-      engineers from our sponsor companies to learn how they are tackling 
-      tough problems, and what interesting solutions they've come up with.
+      Mingle with other engineers, speakers, workshop instructors, and sponsors over food and drinks. Get insight from casual conversation or discuss a specific subject at the topic tables.
     `,
     imgSrc: WelcomeReceptionIcon,
-    imgAlt: 'welcome-reception',
-    ctaUrl: '/schedule'
+    imgAlt: 'Welcome Reception',
+    ctaUrl: '/welcome-reception',
+    ctaText: 'Learn more'
   }, {
     title: 'Topic Tables',
     description: `
-      Informal sessions to get up close and personal with speakers, workshop
-      instructors and other experts on a specific subject. Topic Tables take 
-      place each day and break up the morning & afternoon sessions. 
+      Informal sessions to get up close and personal with speakers, workshop instructors, and other experts on a specific subject.
     `,
     imgSrc: TopicTablesIcon,
-    imgAlt: 'topic-tables',
-    ctaUrl: '/about/topic-tables'
+    imgAlt: 'Topic Tables',
+    ctaUrl: '/topic-tables',
+    ctaText: 'See Full Schedule'
   }, {
-    title: 'Hiring Mixer',
+    title: 'Conference Talks',
     description: `
-      Looking for your next job? Or out to get your first? Join our Remote
-      Hiring Mixer and apply to a number of companies at once! 
+      Two days of talks heavy on content, applicability, and originality. With over 12 speakers discussing a variety of subjects, you're sure to learn many things to implement back at the office.
+    `,
+    imgSrc: ConferenceTalksIcon,
+    imgAlt: 'Conference Talks',
+    ctaUrl: '/conference-talks',
+    ctaText: 'Learn more'
+  }, {
+    title: 'Unconference Sessions',
+    description: `
+      The morning of Day 1 features unconference sessions & topic tables designed to facilitate & share your experience from the past 2 years with other engineers.
+    `,
+    imgSrc: UnconferenceIcon,
+    imgAlt: 'Unconference Sessions',
+    ctaUrl: '/unconference-sessions',
+    ctaText: 'Learn more'
+  }, {
+    title: 'Jobs',
+    description: `
+      Looking for your next job? Or out to get your first? Browse our job listings from some of the top tech companies in the world!
     `,
     imgSrc: HiringMixerIcon,
-    imgAlt: 'hiring-mixer',
-    ctaUrl: '/about/jobs'
+    imgAlt: 'Jobs',
+    ctaUrl: '/jobs',
+    ctaText: 'Learn more'
+  }, {
+    title: 'Workshops',
+    description: `
+      From 4 hour workshops on the basics to full-day workshop on advanced topics, you'll learn in hours what our instructors have mastered over years.
+    `,
+    imgSrc: WorkshopsIcon,
+    imgAlt: 'Workshops',
+    ctaUrl: '/workshops',
+    ctaText: 'Learn more'
   }]
 }
 
