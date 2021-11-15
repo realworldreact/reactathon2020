b760e261f42c6fa5358041506a2647485b394e46
DmytroPopovSSA <dmytro.popov@ssa-outsourcing.com>
2021-11-15 09:00:20 +0200

    Testimonials - added section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import HomeBanner from './Banner'
 import Speakers from './Speakers'
 import Features from './Features'
 import Highlights from './Highlights'
+import Testimonials from './Testimonials'
 import Sponsors from './Sponsors'
 import Divider from '../Divider'
 import './index.css'
@@ -16,6 +17,8 @@ const HomeComponent = () => (
       <Features />
       <Divider className='section-divider' />
       <Highlights />
+      <Divider className='section-divider' />
+      <Testimonials />
       <Divider className='section-divider' />
       <Sponsors />
       <Divider className='section-divider' />
