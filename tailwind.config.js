63766d5918b2a36a23e5f991ace39045dbceab63
DmytroPopovSSA <dmytro.popov@ssa-outsourcing.com>
2022-01-14 22:08:36 +0200

    updated styles according the design with heading

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
   theme: {
     extend: {
       fontSize: {
-        '4_5xl': '2.75rem'
+        '4_5xl': '2.75rem',
+        '5_5xl': '3.5rem'
       },
       colors: {
         wild_watermelon: '#ff6470',
@@ -17,6 +18,9 @@ module.exports = {
       },
       fontFamily: {
         roboto: 'Roboto Slab'
+      },
+      letterSpacing: {
+        '0_25': '0.25em'
       }
     }
   },
