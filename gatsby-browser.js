caa283b482fd637a6ee7f58265a7945d2a976d8e
DmytroPopovSSA <dmytro.popov@ssa-outsourcing.com>
2022-01-19 17:16:10 +0200

    FAQ links prevent scroll to top

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,8 +9,11 @@ import './src/styles/global.css'
 
 export const shouldUpdateScroll = ({ routerProps: { location } }) => {
   const { pathname } = location
-  const scrollToTopRoutes = ['/about/venue-and-hotel', '/about/topic-tables', '/about/jobs', '/about/faq']
-  if (scrollToTopRoutes.indexOf(pathname) === -1) {
+  const notScrollToTopRoutes = [
+    '/about/venue-and-hotel', '/about/topic-tables', '/about/jobs', '/about/faq',
+    '/about/faq/general', '/about/faq/safety', '/about/faq/workshops', '/about/faq/amenities', '/about/faq/logistics', '/about/faq/policies'
+  ]
+  if (notScrollToTopRoutes.indexOf(pathname) === -1) {
     window.scrollTo(0, 0)
   }
   return false
