6b9b4232999f7b2cd8d542cec22291bbef0394d4
DmytroPopovSSA <dmytro.popov@ssa-outsourcing.com>
2022-01-19 14:56:21 +0200

    about page scroll fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,12 @@
 
 // You can delete this file if you're not using it
 import './src/styles/global.css'
+
+export const shouldUpdateScroll = ({ routerProps: { location } }) => {
+  const { pathname } = location
+  const scrollToTopRoutes = ['/about/venue-and-hotel', '/about/topic-tables', '/about/jobs', '/about/faq']
+  if (scrollToTopRoutes.indexOf(pathname) === -1) {
+    window.scrollTo(0, 0)
+  }
+  return false
+}
