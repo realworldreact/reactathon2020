60f0a4a6a8ef4620f2f1260b7b0c19dd6f726f0d
DmytroPopovSSA <dmytro.popov@ssa-outsourcing.com>
2022-01-19 09:53:32 +0200

    header banner fixes & updates

--- src/components/Home/Banner/Content/index.js
+++ src/components/Home/Banner/Content/index.js
@@ -3,10 +3,14 @@ import Link from '../../../Link'
 import { BUY_TICKETS_URI } from '../../../../constants'
 import HeroBannerLogo from '../../../../assets/images/home/header/serverless-in-the-park-logo.svg'
 import PresentingSponsorWhiteLogo from '../../../../assets/images/home/header/vercel-and-rwr-white-logos.svg'
+import ReactathonLogo from '../../../../assets/images/left-nav/reactathon-nav-logo.svg'
 import './index.css'
 
 const HomeBannerContent = () => (
   <div className="home-banner-content">
+    <div className="home-banner-logo home-banner-logo_mobile">
+      <img src={ReactathonLogo} alt="Hero banner logo"/>
+    </div>
     <p>with</p>
     <div className="home-banner-logo">
       <img src={HeroBannerLogo} alt="Hero banner logo"/>
