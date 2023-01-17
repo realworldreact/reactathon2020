ded14c07672634b67fcc04c84c4bfdb6d9e0366c
eduarmreyes <eduar.mreyes@gmail.com>
2023-01-17 19:06:10 +0000

    yarn update and added prettierrc

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,14 @@ module.exports = {
     title: 'Reactathon 2020',
   },
   plugins: [
-      'gatsby-plugin-postcss',
-      'gatsby-plugin-react-helmet',
-      'gatsby-transformer-json',
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `./src/assets/data`,
-        },
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/assets/data',
       },
-    ],
-};
+    },
+  ],
+}
