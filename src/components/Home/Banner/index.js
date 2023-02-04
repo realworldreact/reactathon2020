96cbb2d02860a2feb7450ed7c9498b2f71b64525
eduarmreyes <eduar.mreyes@gmail.com>
2023-02-04 10:50:02 -0800

    add home banner content copy element

--- src/components/Home/Banner/index.js
+++ src/components/Home/Banner/index.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import Banner from '../../Banner'
 import HomeBannerContent from './Content'
+import HomeBannerContentCopy from './Content/HomeBannerContentCopy'
 import './index.css'
 
 import homeVideoPoster from '../../../assets/images/home/header/home-header-placeholder.jpg'
 import homeVideo from '../../../assets/videos/home-header-video.mp4'
 
 const HomeBanner = () => (
-  <Banner
-    video={{ src: homeVideo, poster: homeVideoPoster }}
-    className="home-banner banner-overlay"
-    content={<HomeBannerContent />}
-  />
+  <>
+    <Banner
+      className="banner banner-content banner-copy banner-overlay"
+      content={<HomeBannerContentCopy />}
+    />
+    <Banner
+      video={{ src: homeVideo, poster: homeVideoPoster }}
+      className="home-banner banner-overlay"
+      content={<HomeBannerContent />}
+    />
+  </>
 )
 
 HomeBanner.defaultProps = {
