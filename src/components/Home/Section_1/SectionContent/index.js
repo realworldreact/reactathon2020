73572fcbb9045192661e26db6f256cbd1ca372e3
Benjamin Dunphy <dunphy.ben@gmail.com>
2023-02-05 13:48:55 -0800

    update home page copy

--- src/components/Home/Section_1/SectionContent/index.js
+++ src/components/Home/Section_1/SectionContent/index.js
@@ -3,32 +3,22 @@ import React from 'react'
 const Section1Content = () => {
   return (
     <>
-      {/* <div className="home-section1-subtitle subtitle-section1">There is no substitute for in-person connections</div> */}
+      <div className="home-section1-subtitle subtitle-section1">The most advanced technical React conference</div>
       <div className="section1-text">
         <p>
-          At Reactathon, we facilitate conversations around the latest topics,
-          technologies, and various skill levels to help you find the people &
-          subjects that are most meaningful for you. You will learn from some of
-          the leading engineers in the field who present on stage in a single
-          track. Whether you are a junior engineer looking to learn from senior
-          engineers & thought-leaders, a director of engineering seeking to push
-          the boundaries of your own team, or just someone who loves the energy
-          & network effect of in-person events, Reactathon has something for
-          you.
-        </p>
-        <p>
-          The theme for this year&#39;s conference is performance. With various
-          methods & patterns, new tech and new features for older tech, to
-          shipping less JavaScript to the client, there are many ways to speed
-          up your existing app to improve user experience and increase
-          conversions.
-        </p>
-        <p>
-          You can read about our{' '}
-          <a href="/about/faq/safety">
-            entry requirements and safety measures here
-          </a>
-          .
+          If you're looking for a technical conference on React &amp; the modern 
+          frontend ecosystem, look no further than Reactathon. We facilitate 
+          conversations around the latest topics, technologies, and various skill 
+          levels to help you find the people &amp; subjects that are most meaningful
+          for you. You will learn from some of the leading engineers in the field
+          who present on stage in a single track. And you will have plenty of opportunites
+          to meet &amp; discuss with those speakers and other experts including library
+          maintainers, core-contributors, &amp; sponsor companies working on interesting
+          &amp; relevant problems. Whether you are a junior engineer
+          looking to learn from senior engineers &amp; thought-leaders, a director
+          of engineering seeking to push the boundaries of your own team, or just
+          someone who loves the energy &amp; network effect of in-person events, 
+          Reactathon has something for you.
         </p>
       </div>
     </>
