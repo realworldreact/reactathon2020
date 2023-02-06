7f4c90c4b9ce5235dde96a098d61aa63d289d87f
eduarmreyes <eduar.mreyes@gmail.com>
2023-02-05 21:05:12 -0800

    comment out workshops and show workshops coming soon title

--- src/components/Workshops/index.js
+++ src/components/Workshops/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import WorkshopsBanner from './Banner'
-import WorkshopsSectionWrapper from './Section/Wrapper'
+// import WorkshopsSectionWrapper from './Section/Wrapper'
 import './index.css'
 
 const Schedule = () => (
-  <section className='section-workshops'>
+  <section className="section-workshops">
     <WorkshopsBanner />
-    <section className='section-content section-workshops-content'>
+    <h3 className="workshop-coming-soon">Workshops coming soon</h3>
+    {/* <section className='section-content section-workshops-content'>
       <WorkshopsSectionWrapper />
-    </section>
+    </section> */}
   </section>
 )
 
