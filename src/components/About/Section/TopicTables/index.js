95c55e7faa1ed337bf043f8f8ccd70e19321bc80
eduarmreyes <eduar.mreyes@gmail.com>
2023-02-05 21:16:29 -0800

    topic tables coming soon

--- src/components/About/Section/TopicTables/index.js
+++ src/components/About/Section/TopicTables/index.js
@@ -5,36 +5,41 @@ import Topics from './Topics'
 import data from '../../../../assets/data/about/topic-tables.json'
 import './index.css'
 
-
 const TopicTablesDateTime = ({ date, time }) => (
   <AboutSectionDateTime
     date={date}
     time={time}
-    className='topic-tables-datetime'
+    className="topic-tables-datetime"
   />
 )
 
 const TopicTableList = ({ items = [] }) => (
   <div>
-    <ul className='topic-tables-list'>
+    <ul className="topic-tables-list">
       {items.map((item, idx) => (
-        <li key={idx}>
-          {item}
-        </li>
+        <li key={idx}>{item}</li>
       ))}
     </ul>
   </div>
 )
 
-const TopicTables = ({ header, description, topicHeader, listItems, date, time }) => (
-  <section className='topic-tables'>
-    <SubPageSectionHeader text={header} className='topic-tables-header' />
-    <p className='topic-tables-subtext'>
-      {description}
-    </p>
-    <TopicTablesDateTime date={date} time={time} />
+const TopicTables = ({
+  header,
+  description,
+  topicHeader,
+  listItems,
+  date,
+  time,
+}) => (
+  <section className="topic-tables">
+    <SubPageSectionHeader text={header} className="topic-tables-header" />
+    <p className="topic-tables-subtext">{description}</p>
+    <h3 className="topic-tables-coming-soon">
+      Topic Tables to be announced soon
+    </h3>
+    {/* <TopicTablesDateTime date={date} time={time} />
     <TopicTableList items={listItems} />
-    <Topics />
+    <Topics /> */}
   </section>
 )
 
@@ -43,7 +48,7 @@ TopicTables.defaultProps = {
   description: data.section.p1,
   listItems: data.section.bullets,
   date: data.section.date,
-  time: data.section.time
+  time: data.section.time,
 }
 
 export default TopicTables
