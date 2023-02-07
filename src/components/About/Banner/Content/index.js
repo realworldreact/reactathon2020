851f57965b623a535206cdde90207f3c1cf13e71
Benjamin Dunphy <dunphy.ben@gmail.com>
2023-02-07 15:47:03 -0800

    update speakers info and also faq copy

--- src/components/About/Banner/Content/index.js
+++ src/components/About/Banner/Content/index.js
@@ -20,11 +20,7 @@ const AboutBannerContent = ({ title, description }) => (
 AboutBannerContent.defaultProps = {
   title: 'About',
   description: `
-  Since 2017, Reactathon has brought frontend and fullstack engineers together to learn,
-  connect, and inspire. For 2022, we return in-person at a beautiful outdoor venue in
-  San Francisco's East Bay. With the addition of the optional Serverless in the Park on May 5,
-  you'll enjoy 2.5 days of talks from industry leaders, 6 different workshops, and true
-  connections with your peers. There's so much to look forward to & experience! 
+  Since 2017, Reactathon has brought frontend and fullstack engineers together to learn, connect, and inspire. For 2023, we return to San Francisco in the beautiful, clean, and safe neighborhoods of Pacific Heights & Japantown. Join us for advanced technical presentations, facilitated conversation, & an abundance of webdev fun! 
   `
 }
 
