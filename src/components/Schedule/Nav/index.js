d9fe33f7445cf523af01f3307940ea8eee2f23df
eduarmreyes <eduar.mreyes@gmail.com>
2023-03-11 21:40:15 -0800

    schedule nav be 50%

--- src/components/Schedule/Nav/index.js
+++ src/components/Schedule/Nav/index.js
@@ -4,36 +4,40 @@ import { getLocationHash, getLocationPathname } from '../../../utils/window'
 import './index.css'
 
 const ScheduleNav = ({ items }) => (
-  <div className='schedule-nav'>
+  <div className="schedule-nav">
     <ContentNavMenu
       items={items.map(item => ({
         ...item,
         className: 'schedule-content-nav-item',
         activeLinkClassName: 'schedule-content-nav-item-is-active',
-        isActiveHandler: () =>  {
+        isActiveHandler: () => {
           return item.href === `${getLocationPathname()}${getLocationHash()}`
-        }
+        },
       }))}
       showDivider={true}
-      dividerClassName='schedule-content-nav-divider'
+      dividerClassName="schedule-content-nav-divider"
     />
   </div>
 )
 
 ScheduleNav.defaultProps = {
-  items: [{
-    id: 'day1',
-    text: 'May 3',
-    href: '/schedule/day-1',
-  }, {
-    id: 'day2',
-    text: 'May 4',
-    href: '/schedule/day-2',
-  }, {
-    id: 'day3',
-    text: 'May 5',
-    href: '/schedule/day-3'
-  }]
+  items: [
+    {
+      id: 'day1',
+      text: 'May 2',
+      href: '/schedule/day-1',
+    },
+    {
+      id: 'day2',
+      text: 'May 3',
+      href: '/schedule/day-2',
+    },
+    // {
+    //   id: 'day3',
+    //   text: 'May 4',
+    //   href: '/schedule/day-3',
+    // },
+  ],
 }
 
 export default ScheduleNav
